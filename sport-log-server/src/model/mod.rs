8f9b954927da51a16f2f0f9963aa940dd534e13b
Lorenz Schüler <lorenz@saarow.de>
2021-07-14 11:28:44 +0200

    add mod model::strength

--- sport-log-server/src/model/mod.rs
+++ sport-log-server/src/model/mod.rs
@@ -4,6 +4,7 @@ mod diary_wod;
 mod metcon;
 mod movement;
 mod platform;
+mod strength;
 mod user;
 
 pub use action::*;
@@ -12,4 +13,5 @@ pub use diary_wod::*;
 pub use metcon::*;
 pub use movement::*;
 pub use platform::*;
+pub use strength::*;
 pub use user::*;
---
+++ sport-log-server/src/model/strength.rs
@@ -0,0 +1,56 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+
+use sport_log_server_derive::{Create, Delete, GetAll, GetById, Update};
+
+use crate::schema::{strength_session, strength_set};
+
+use super::{MovementId, MovementUnit, UserId};
+
+pub type StrengthSessionId = i32;
+
+#[derive(
+    Queryable, AsChangeset, Serialize, Deserialize, Debug, Create, GetById, GetAll, Update, Delete,
+)]
+#[table_name = "strength_session"]
+pub struct StrengthSession {
+    pub id: StrengthSessionId,
+    pub user_id: UserId,
+    pub datetime: NaiveDateTime,
+    pub movement_id: MovementId,
+    pub movement_unit: MovementUnit,
+    pub interval: Option<i32>,
+    pub comments: Option<String>,
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "strength_session"]
+pub struct NewStrengthSession {
+    pub user_id: UserId,
+    pub datetime: NaiveDateTime,
+    pub movement_id: MovementId,
+    pub movement_unit: MovementUnit,
+    pub interval: Option<i32>,
+    pub comments: Option<String>,
+}
+
+pub type StrengthSetId = i32;
+
+#[derive(
+    Queryable, AsChangeset, Serialize, Deserialize, Debug, Create, GetById, GetAll, Update, Delete,
+)]
+#[table_name = "strength_set"]
+pub struct StrengthSet {
+    pub id: StrengthSetId,
+    pub strength_session_id: StrengthSessionId,
+    pub count: i32,
+    pub weight: Option<f32>,
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "strength_set"]
+pub struct NewStrengthSet {
+    pub strength_session_id: StrengthSessionId,
+    pub count: i32,
+    pub weight: Option<f32>,
+}
