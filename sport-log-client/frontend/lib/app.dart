c3ae08d372f1efc09f036f752174f508f9504436
Oliver Portee <oliver.portee@gmail.com>
2021-07-24 20:33:40 +0200

    move pages into seperate pages directory

--- sport-log-client/frontend/lib/app.dart
+++ sport-log-client/frontend/lib/app.dart
@@ -1,13 +1,10 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sport_log/authentication/authentication_bloc.dart';
 import 'package:sport_log/authentication/protected_route.dart';
-import 'package:sport_log/home/home_page.dart';
-import 'package:sport_log/landing/landing_page.dart';
-
-import 'login/login_page.dart';
-import 'registration/registrations_page.dart';
+import 'package:sport_log/pages/home/home_page.dart';
+import 'package:sport_log/pages/landing/landing_page.dart';
+import 'package:sport_log/pages/login/login_page.dart';
+import 'package:sport_log/pages/registration/registrations_page.dart';
 import 'routes.dart';
 
 class App extends StatefulWidget {
--- sport-log-client/frontend/lib/pages/login/login_page.dart
+++ sport-log-client/frontend/lib/pages/login/login_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:repositories/authentication_repository.dart';
 import 'package:sport_log/authentication/authentication_bloc.dart';
-import 'package:sport_log/login/login_bloc.dart';
+import 'package:sport_log/pages/login/login_bloc.dart';
 import 'package:sport_log/routes.dart';
 
 class LoginPage extends StatefulWidget {
