eb77b045930864dd218a167f9179b5f48b017e1c
Oliver Portee <oliver.portee@gmail.com>
2021-07-25 01:17:36 +0200

    logging storage activity to console

--- sport-log-client/frontend/lib/repositories/authentication_repository.dart
+++ sport-log-client/frontend/lib/repositories/authentication_repository.dart
@@ -1,4 +1,6 @@
 
+import 'dart:developer';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:sport_log/models/user.dart';
 
@@ -7,18 +9,27 @@ class AuthenticationRepository {
   final storage = const FlutterSecureStorage();
 
  Future<void> deleteUser() async {
+  log("deleting user data from secure storage...");
   for (final key in User.allKeys) {
    storage.delete(key: key);
   }
  }
 
  Future<void> createUser(User user) async {
+  log("saving user data in secure storage...");
   for (final entry in user.toMap().entries) {
    storage.write(key: entry.key, value: entry.value);
   }
  }
 
  Future<User?> getUser() async {
-  return User.fromMap(await storage.readAll());
+  log("reading user data from secure storage...");
+  final user = User.fromMap(await storage.readAll());
+  if (user == null) {
+   log("no user data found");
+  } else {
+   log("user data found");
+  }
+  return user;
  }
 }
