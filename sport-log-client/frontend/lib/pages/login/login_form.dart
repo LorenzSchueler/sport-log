825575a817fa02161662536d3e021f5a2c02db64
Oliver Portee <oliver.portee@gmail.com>
2021-07-25 21:48:27 +0200

    improve login form

---
+++ sport-log-client/frontend/lib/pages/login/login_form.dart
@@ -0,0 +1,153 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sport_log/routes.dart';
+
+import 'login_bloc.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => LoginFormState();
+}
+
+class LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = "";
+  String _password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: BlocConsumer<LoginBloc, LoginState>(
+        listener: (context, state) {
+          if (state == LoginState.successful) {
+            Navigator.of(context).pushNamedAndRemoveUntil(Routes.home,
+                    (route) => false);
+          } else if (state == LoginState.failed) {
+            const snackBar = SnackBar(
+                content: Text("Invalid credentials. Please try again."),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+        },
+        builder: (context, state) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _usernameInput(context, state),
+              _padding,
+              _passwordInput(context, state),
+              _padding,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  _submitButton(context, state),
+                ],
+              )
+            ],
+          );
+        }
+      ),
+    );
+  }
+
+  Widget _usernameInput(BuildContext context, LoginState state) {
+    return TextFormField(
+      onChanged: (username) {
+        setState(() {
+          _username = username;
+        });
+      },
+      decoration: const InputDecoration(
+        labelText: "Username",
+        border: OutlineInputBorder(
+            borderRadius: _borderRadius
+        ),
+      ),
+      validator: _usernameValidator,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      enabled: _inputsEnabled(state),
+      style: _inputsEnabled(state) ? null : TextStyle(
+        color: Theme.of(context).disabledColor
+      ),
+    );
+  }
+
+  Widget _passwordInput(BuildContext context, LoginState state) {
+    return TextFormField(
+      onChanged: (password) {
+        setState(() {
+          _password = password;
+        });
+      },
+      decoration: const InputDecoration(
+        labelText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: _borderRadius
+        ),
+      ),
+      validator: _passwordValidator,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      enabled: _inputsEnabled(state),
+      style: _inputsEnabled(state) ? null : TextStyle(
+          color: Theme.of(context).disabledColor
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget _submitButton(BuildContext context, LoginState state) {
+    return ElevatedButton(
+      child: (state != LoginState.pending)
+          ? const Text("Login") : const CircularProgressIndicator(),
+      onPressed: (state != LoginState.successful && _inputsAreValid)
+          ? () => _submit(context) : null,
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: _borderRadius,
+        ),
+      ),
+    );
+  }
+
+  bool _inputsEnabled(LoginState state) {
+    return state != LoginState.pending;
+  }
+
+  bool get _formIsValid => _formKey.currentState!.validate();
+
+  void _submit(BuildContext context) {
+    if (_formIsValid) {
+      context.read<LoginBloc>().add(SubmitLogin(
+          username: _username,
+          password: _password
+      ));
+      _formKey.currentState!.deactivate();
+    }
+  }
+
+  String? _usernameValidator(String? username) {
+    if (username != null && username.isEmpty) {
+      return "Username must not be empty.";
+    }
+  }
+
+  String? _passwordValidator(String? password) {
+    if (password != null && password.isEmpty) {
+      return "Password must not be empty";
+    }
+  }
+
+  bool get _inputsAreValid =>
+    _usernameValidator(_username) == null
+    && _passwordValidator(_password) == null;
+
+  static const Widget _padding = Padding(
+      padding: EdgeInsets.all(10),
+  );
+  
+  static const BorderRadius _borderRadius = BorderRadius.all(Radius.circular(20));
+}
--- sport-log-client/frontend/lib/pages/login/login_page.dart
+++ sport-log-client/frontend/lib/pages/login/login_page.dart
@@ -1,24 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sport_log/pages/login/login_form.dart';
 import 'package:sport_log/repositories/authentication_repository.dart';
 import 'package:sport_log/authentication/authentication_bloc.dart';
 import 'package:sport_log/pages/login/login_bloc.dart';
-import 'package:sport_log/routes.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage> {
-  bool _usernameIsPure = true;
-  final TextEditingController _usernameController = TextEditingController();
-
-  bool _passwordIsPure = true;
-  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,60 +22,7 @@ class LoginPageState extends State<LoginPage> {
               authenticationRepository: context.read<AuthenticationRepository>(),
             );
           },
-          child: BlocConsumer<LoginBloc, LoginState>(
-            listener: (context, state) {
-              if (state == LoginState.successful) {
-                Navigator.of(context).pushNamedAndRemoveUntil(Routes.home,
-                        (route) => false);
-              }
-            },
-            builder: (context, state) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: _usernameController,
-                    onChanged: (username) {
-                      setState(() {
-                        _usernameIsPure = false;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Username",
-                      errorText: (!_usernameIsPure && _usernameController.text.isEmpty)
-                          ? "Username must not be empty." : null,
-                    ),
-                  ),
-                  const Padding(padding: EdgeInsets.all(12)),
-                  TextFormField(
-                    controller: _passwordController,
-                    onChanged: (username) {
-                      setState(() {
-                        _passwordIsPure = false;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      errorText: (!_passwordIsPure && _passwordController.text.isEmpty)
-                          ? "Password must not be empty." : null,
-                    ),
-                    obscureText: true,
-                  ),
-                  const Padding(padding: EdgeInsets.all(12)),
-                  ElevatedButton(
-                    onPressed: (state == LoginState.idle || state == LoginState.failed)
-                      ? () {
-                        context.read<LoginBloc>().add(SubmitLogin(username: _usernameController.text, password: _passwordController.text));
-                      }
-                      : null,
-                    child: (state == LoginState.idle || state == LoginState.failed)
-                      ? const Text("Login")
-                      : const CircularProgressIndicator(),
-                  ),
-                ],
-              );
-            },
-          ),
+          child: const LoginForm(),
         ),
       ),
     );
