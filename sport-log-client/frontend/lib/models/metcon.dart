402652e069b19c2eb0c8a980e8361fdbecc07baf
Oliver Portee <oliver.portee@gmail.com>
2021-07-28 18:18:17 +0200

    create metcon repository

--- sport-log-client/frontend/lib/models/metcon.dart
+++ sport-log-client/frontend/lib/models/metcon.dart
@@ -5,6 +5,56 @@ enum MetconType {
   amrap, emom, forTime
 }
 
+class MetconMovement {
+  MetconMovement({
+    required this.id,
+    required this.movementId,
+    required this.count,
+    required this.unit,
+    required this.weight,
+  });
+
+  int id;
+  int movementId;
+  int count;
+  MovementUnit unit;
+  double weight;
+
+  MetconMovement.fromNewMetconMovement(NewMetconMovement nmm, this.id)
+    : movementId = nmm.movementId,
+      count = nmm.count,
+      unit = nmm.unit,
+      weight = nmm.weight;
+}
+
+class Metcon {
+  Metcon({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.rounds,
+    required this.timecap,
+    required this.moves,
+  });
+
+  int id;
+  String name;
+  MetconType type;
+  int rounds;
+  Duration timecap;
+
+  List<MetconMovement> moves;
+
+  Metcon.fromNewMetconWithMoves(
+      NewMetcon nm,
+      this.id,
+      this.moves
+  ) : name = nm.name,
+      type = nm.type,
+      rounds = nm.rounds,
+      timecap = nm.timecap;
+}
+
 class NewMetconMovement {
   NewMetconMovement({
     required this.movementId,
---
+++ sport-log-client/frontend/lib/repositories/metcon_repository.dart
@@ -0,0 +1,33 @@
+
+import 'package:sport_log/models/metcon.dart';
+
+class MetconRepository {
+  final Map<int, Metcon> _metcons = {};
+
+  int get _nextId => _metcons.length;
+  int get _nextMovementId => _metcons.values.toList().fold(0,
+          (int number, m) => number + m.moves.length);
+
+  void createMetcon(NewMetcon newMetcon) {
+    final mms = newMetcon.moves.map((nmm) =>
+        MetconMovement.fromNewMetconMovement(nmm, _nextMovementId)).toList();
+    final id = _nextId;
+    final metcon = Metcon.fromNewMetconWithMoves(newMetcon, id, mms);
+    _metcons[id] = metcon;
+  }
+
+  void deleteMetcon(int id) {
+    _metcons.remove(id);
+  }
+
+  Metcon? getMetcon(int id) {
+    if (!_metcons.containsKey(id)) {
+      return null;
+    }
+    return _metcons[id];
+  }
+
+  List<Metcon> getAllMetcons() {
+    return _metcons.values.toList();
+  }
+}
