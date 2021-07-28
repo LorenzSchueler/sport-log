42a3e4f2457e378b024cdcfcfe22757092a42435
Oliver Portee <oliver.portee@gmail.com>
2021-07-28 17:35:03 +0200

    create movement types

--- sport-log-client/frontend/lib/models/movement.dart
+++ sport-log-client/frontend/lib/models/movement.dart
@@ -2,3 +2,33 @@
 enum MovementUnit {
   reps, cal, meter, km, yard, foot, mile
 }
+
+enum MovementCategory {
+  strength, cardio
+}
+
+class NewMovement {
+  NewMovement({
+    required this.name,
+    required this.description,
+    required this.category,
+  });
+
+  String name;
+  String description;
+  MovementCategory category;
+}
+
+class Movement {
+  Movement({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.category,
+  });
+
+  int id;
+  String name;
+  String description;
+  MovementCategory category;
+}
