6639ee6e03e4b794e6a371a81dc3709621611d5e
Lorenz Schüler <lorenz@saarow.de>
2021-07-31 17:57:40 +0200

    sportstracker: get and parse all data

--- sport-log-action-provider-sportstracker/src/main.rs
+++ sport-log-action-provider-sportstracker/src/main.rs
@@ -1,13 +1,13 @@
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use serde_json::{self, Value};
+
+use sport_log_types::ExecutableActionEvent;
 
 use sport_log_action_provider_sportstracker_config::Config;
-use sport_log_types::ExecutableActionEvent;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
-    sessionkey: Option<String>,
+    sessionkey: Option<String>, // None if login fails
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -91,95 +91,81 @@ async fn main() {
         .json()
         .await
         .unwrap();
-    println!("{:#?}", exec_action_events);
+    println!("executable action events: {}\n", exec_action_events.len());
 
-    for action_event in exec_action_events {
-        if let Some(()) = get_data(&action_event.username, &action_event.password).await {
-            client
-                .delete(format!(
-                    "{}/v1/ap/action_event/{}",
-                    config.base_url, action_event.action_event_id.0
-                ))
-                .basic_auth(&config.username, Some(&config.password))
-                .send()
-                .await
-                .unwrap();
+    for exec_action_event in exec_action_events {
+        println!("{:#?}", exec_action_event);
+        // TODO oldest entry
+        if let Some(data) = get_data(&exec_action_event.username, &exec_action_event.password).await
+        {
+            for (_workout, _workout_data) in data {
+                // insert data into db
+            }
+            //client
+            //.delete(format!(
+            //"{}/v1/ap/action_event/{}",
+            //config.base_url, action_event.action_event_id.0
+            //))
+            //.basic_auth(&config.username, Some(&config.password))
+            //.send()
+            //.await
+            //.unwrap();
+        } else {
+            println!("login failed!\n");
         }
     }
 }
 
-async fn get_data(username: &str, password: &str) -> Option<()> {
-    let credentials = [("l", username), ("p", password)];
-
+async fn get_data(username: &str, password: &str) -> Option<Vec<(Workout, WorkoutData)>> {
     let client = Client::new();
 
-    let login = client
+    let credentials = [("l", username), ("p", password)];
+    let user: User = client
         .post("https://api.sports-tracker.com/apiserver/v1/login")
         .form(&credentials)
         .send()
         .await
         .unwrap()
-        .json::<Value>()
+        .json()
         .await
         .unwrap();
 
-    println!("{}", serde_json::to_string_pretty(&login).unwrap());
-    let sessionkey = login.get("sessionkey")?.as_str().unwrap();
-    let token = ("token", sessionkey);
-
-    let workouts = client
+    let sessionkey = user.sessionkey?;
+    let token = ("token", sessionkey.as_str());
+    let workouts: Workouts = client
         .get("https://api.sports-tracker.com/apiserver/v1/workouts")
         .query(&[token])
         .send()
         .await
-        .unwrap()
-        .json::<Value>()
+        .ok()?
+        .json()
         .await
-        .unwrap();
+        .ok()?;
+    println!("{:#?}", workouts.payload[0]);
 
-    let payload = &workouts["payload"];
-    let body = &payload[0];
-    println!("{}", body["startPosition"]);
-    println!("{}", body["stopPosition"]);
-    println!("{}", body["centerPosition"]);
-    println!("{}", body["workoutKey"]);
-    println!("{}", body["startTime"]);
-    println!("{}", body["stopTime"]);
-    println!("{}", body["totalTime"]);
-    println!("{}", body["totalTime"]);
-    println!("{}", body["totalAscent"]);
-    println!("{}", body["totalDescent"]);
-    println!("{}", body["totalDistance"]);
-    println!("{}", body["stepCount"]);
-    println!("{}", body["minAltitude"]);
-    println!("{}", body["maxAltitude"]);
-    println!("{}", body["energyConsumption"]);
-    println!("{}", body["cadence"]);
-    println!("{}", body["hrdata"]);
-    println!("{}", body["extensions"][0]["temperature"]);
-
-    let workout_id = body["workoutKey"].as_str().unwrap();
     let samples = ("samples", "100000");
 
-    let response = client
-        .get(format!(
-            "https://api.sports-tracker.com/apiserver/v1/workouts/{}/data",
-            workout_id,
-        ))
-        .query(&[token, samples])
-        .send()
-        .await
-        .unwrap()
-        .json::<Value>()
-        .await
-        .unwrap();
+    let mut workout_datas = vec![];
+    for workout in &workouts.payload {
+        workout_datas.push(
+            client
+                .get(format!(
+                    "https://api.sports-tracker.com/apiserver/v1/workouts/{}/data",
+                    workout.workoutKey
+                ))
+                .query(&[token, samples])
+                .send()
+                .await
+                .ok()?
+                .json()
+                .await
+                .ok()?,
+        );
+    }
 
-    let payload = &response["payload"];
-    let points = &payload["locations"];
-    let point0 = &points[0];
-    println!("{}", serde_json::to_string_pretty(&point0).unwrap());
+    let data = workouts.payload.into_iter().zip(workout_datas).collect();
 
-    Some(())
+    Some(data)
 }
 
 // get workout stats: https://api.sports-tracker.com/apiserver/v1/workouts/<workout_id>?token=sessionkey
