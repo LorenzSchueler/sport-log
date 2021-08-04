dc92d6aa6528bf2bfcdf9ac4e5952312f35e9275
Oliver Portee <oliver.portee@gmail.com>
2021-08-04 13:51:54 +0200

    overhaul metcon models completely

--- sport-log-client/frontend/lib/models/metcon.dart
+++ sport-log-client/frontend/lib/models/metcon.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:sport_log/models/movement.dart';
 
 enum MetconType {
@@ -18,88 +19,125 @@ extension ToDisplayName on MetconType {
   }
 }
 
-class MetconMovement {
-  MetconMovement({
-    required this.id,
-    required this.movementId,
-    required this.count,
-    required this.unit,
-    this.weight,
-  });
-
-  int id;
-  int movementId;
-  int count;
-  MovementUnit unit;
-  double? weight;
-
-  MetconMovement.fromNewMetconMovement(NewMetconMovement nmm, this.id)
-    : movementId = nmm.movementId,
-      count = nmm.count,
-      unit = nmm.unit,
-      weight = nmm.weight;
-}
-
 class Metcon {
   Metcon({
     required this.id,
-    required this.name,
+    this.userId,
+    this.name,
+    required this.type,
+    this.rounds,
+    this.timecap,
+    this.description
+  });
+
+  int id;
+  int? userId;
+  String? name;
+  MetconType type;
+  int? rounds;
+  int? timecap; // seconds
+  String? description;
+}
+
+class MetconMovement {
+  MetconMovement({
+    required this.id,
+    required this.metconId,
+    required this.movementId,
+    required this.movementNumber,
+    required this.count,
+    required this.unit,
+    this.weight,
+  });
+
+  int id;
+  int metconId;
+  int movementId;
+  int movementNumber; // index within metcon
+  int count;
+  MovementUnit unit;
+  double? weight;
+}
+
+class UiMetcon {
+  UiMetcon({
+    this.id,
+    this.userId,
+    this.name,
     required this.type,
     this.rounds,
     this.timecap,
     this.description,
-    required this.moves,
+    this.moves = const [],
   });
 
-  int id;
-  String name;
+  int? id;
+  int? userId;
+  String? name;
   MetconType type;
   int? rounds;
   Duration? timecap;
   String? description;
+  List<UiMetconMovement> moves;
 
-  List<MetconMovement> moves;
+  UiMetcon.fromMetcon(Metcon m, this.moves)
+    : id = m.id,
+      userId = m.userId,
+      name = m.name,
+      type = m.type,
+      rounds = m.rounds,
+      timecap = (m.timecap != null) ? Duration(seconds: m.timecap!) : null,
+      description = m.description;
 
-  Metcon.fromNewMetconWithMoves(
-      NewMetcon nm,
-      this.id,
-      this.moves
-  ) : name = nm.name,
-      type = nm.type,
-      rounds = nm.rounds,
-      timecap = nm.timecap,
-      description = nm.description;
+  Metcon toMetcon() {
+    assert(id != null);
+    return Metcon(
+      id: id!,
+      userId: userId,
+      name: name,
+      type: type,
+      rounds: rounds,
+      timecap: timecap?.inSeconds,
+      description: description,
+    );
+  }
 }
 
-class NewMetconMovement {
-  NewMetconMovement({
+class UiMetconMovement {
+  UiMetconMovement({
+    this.id,
+    this.metconId,
     required this.movementId,
     required this.count,
     required this.unit,
     this.weight,
   });
 
+  int? id;
+  int? metconId;
   int movementId;
   int count;
   MovementUnit unit;
   double? weight;
-}
 
-class NewMetcon {
-  NewMetcon({
-    required this.name,
-    required this.type,
-    this.rounds,
-    this.timecap,
-    this.description,
-    required this.moves,
-  });
+  UiMetconMovement.fromMetconMovement(MetconMovement mm)
+    : id = mm.id,
+      metconId = mm.metconId,
+      movementId = mm.movementId,
+      count = mm.count,
+      unit = mm.unit,
+      weight = mm.weight;
 
-  String name;
-  MetconType type;
-  int? rounds;
-  Duration? timecap;
-  String? description;
-
-  List<NewMetconMovement> moves;
+  MetconMovement toMetconMovement(Metcon metcon, int movementNumber) {
+    assert(id != null);
+    return MetconMovement(
+      id: id!,
+      metconId: metcon.id,
+      movementId: movementId,
+      movementNumber: movementNumber,
+      count: count,
+      unit: unit,
+      weight: weight,
+    );
+  }
 }
