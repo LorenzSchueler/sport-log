65359df35f9c84b4760cb20689730ebc3b0a5884
Oliver Portee <oliver.portee@gmail.com>
2021-08-07 21:26:04 +0200

    add metcon movement type

---
+++ sport-log-client/frontend/lib/models/metcon/metcon_movement.dart
@@ -0,0 +1,51 @@
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:sport_log/models/movement/movement.dart';
+
+part 'metcon_movement.g.dart';
+
+@JsonSerializable()
+class MetconMovement {
+  MetconMovement({
+    required this.id,
+    required this.metconId,
+    required this.movementId,
+    required this.movementNumber,
+    required this.count,
+    required this.unit,
+    required this.weight,
+  });
+
+  int id;
+  int metconId;
+  int movementId;
+  int movementNumber;
+  int count;
+  MovementUnit unit;
+  double? weight;
+
+  factory MetconMovement.fromJson(Map<String, dynamic> json) => _$MetconMovementFromJson(json);
+  Map<String, dynamic> toJson() => _$MetconMovementToJson(this);
+}
+
+@JsonSerializable()
+class NewMetconMovement {
+  NewMetconMovement({
+    required this.metconId,
+    required this.movementId,
+    required this.movementNumber,
+    required this.count,
+    required this.unit,
+    required this.weight,
+  });
+
+  int metconId;
+  int movementId;
+  int movementNumber;
+  int count;
+  MovementUnit unit;
+  double? weight;
+
+  factory NewMetconMovement.fromJson(Map<String, dynamic> json) => _$NewMetconMovementFromJson(json);
+  Map<String, dynamic> toJson() => _$NewMetconMovementToJson(this);
+}
---
+++ sport-log-client/frontend/lib/models/metcon/metcon_movement.g.dart
@@ -0,0 +1,85 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'metcon_movement.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MetconMovement _$MetconMovementFromJson(Map<String, dynamic> json) =>
+    MetconMovement(
+      id: json['id'] as int,
+      metconId: json['metcon_id'] as int,
+      movementId: json['movement_id'] as int,
+      movementNumber: json['movement_number'] as int,
+      count: json['count'] as int,
+      unit: _$enumDecode(_$MovementUnitEnumMap, json['unit']),
+      weight: (json['weight'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$MetconMovementToJson(MetconMovement instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'metcon_id': instance.metconId,
+      'movement_id': instance.movementId,
+      'movement_number': instance.movementNumber,
+      'count': instance.count,
+      'unit': _$MovementUnitEnumMap[instance.unit],
+      'weight': instance.weight,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$MovementUnitEnumMap = {
+  MovementUnit.reps: 'Reps',
+  MovementUnit.cal: 'Cal',
+  MovementUnit.meter: 'Meter',
+  MovementUnit.km: 'Km',
+  MovementUnit.yard: 'Yard',
+  MovementUnit.foot: 'Foot',
+  MovementUnit.mile: 'Mile',
+};
+
+NewMetconMovement _$NewMetconMovementFromJson(Map<String, dynamic> json) =>
+    NewMetconMovement(
+      metconId: json['metcon_id'] as int,
+      movementId: json['movement_id'] as int,
+      movementNumber: json['movement_number'] as int,
+      count: json['count'] as int,
+      unit: _$enumDecode(_$MovementUnitEnumMap, json['unit']),
+      weight: (json['weight'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$NewMetconMovementToJson(NewMetconMovement instance) =>
+    <String, dynamic>{
+      'metcon_id': instance.metconId,
+      'movement_id': instance.movementId,
+      'movement_number': instance.movementNumber,
+      'count': instance.count,
+      'unit': _$MovementUnitEnumMap[instance.unit],
+      'weight': instance.weight,
+    };
