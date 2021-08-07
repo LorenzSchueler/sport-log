572ed0fb542d409a988b4b49fbfe5f75a016aa15
Oliver Portee <oliver.portee@gmail.com>
2021-08-07 21:20:24 +0200

    add movement model type

---
+++ sport-log-client/frontend/lib/models/movement/movement.dart
@@ -0,0 +1,57 @@
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'movement.g.dart';
+
+enum MovementCategory {
+  @JsonValue("Cardio") cardio,
+  @JsonValue("Strength") strength,
+}
+
+enum MovementUnit {
+  @JsonValue("Reps") reps,
+  @JsonValue("Cal") cal,
+  @JsonValue("Meter") meter,
+  @JsonValue("Km") km,
+  @JsonValue("Yard") yard,
+  @JsonValue("Foot") foot,
+  @JsonValue("Mile") mile,
+}
+
+@JsonSerializable()
+class Movement {
+  Movement({
+    required this.id,
+    required this.userId,
+    required this.name,
+    required this.description,
+    required this.category,
+  });
+
+  int id;
+  int? userId;
+  String name;
+  String? description;
+  MovementCategory category;
+
+  factory Movement.fromJson(Map<String, dynamic> json) => _$MovementFromJson(json);
+  Map<String, dynamic> toJson() => _$MovementToJson(this);
+}
+
+@JsonSerializable()
+class NewMovement {
+  NewMovement({
+    required this.userId,
+    required this.name,
+    required this.description,
+    required this.category,
+  });
+
+  int? userId;
+  String name;
+  String? description;
+  MovementCategory category;
+
+  factory NewMovement.fromJson(Map<String, dynamic> json) => _$NewMovementFromJson(json);
+  Map<String, dynamic> toJson() => _$NewMovementToJson(this);
+}
---
+++ sport-log-client/frontend/lib/models/movement/movement.g.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movement.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Movement _$MovementFromJson(Map<String, dynamic> json) => Movement(
+      id: json['id'] as int,
+      userId: json['user_id'] as int?,
+      name: json['name'] as String,
+      description: json['description'] as String?,
+      category: _$enumDecode(_$MovementCategoryEnumMap, json['category']),
+    );
+
+Map<String, dynamic> _$MovementToJson(Movement instance) => <String, dynamic>{
+      'id': instance.id,
+      'user_id': instance.userId,
+      'name': instance.name,
+      'description': instance.description,
+      'category': _$MovementCategoryEnumMap[instance.category],
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$MovementCategoryEnumMap = {
+  MovementCategory.cardio: 'Cardio',
+  MovementCategory.strength: 'Strength',
+};
+
+NewMovement _$NewMovementFromJson(Map<String, dynamic> json) => NewMovement(
+      userId: json['user_id'] as int?,
+      name: json['name'] as String,
+      description: json['description'] as String?,
+      category: _$enumDecode(_$MovementCategoryEnumMap, json['category']),
+    );
+
+Map<String, dynamic> _$NewMovementToJson(NewMovement instance) =>
+    <String, dynamic>{
+      'user_id': instance.userId,
+      'name': instance.name,
+      'description': instance.description,
+      'category': _$MovementCategoryEnumMap[instance.category],
+    };
