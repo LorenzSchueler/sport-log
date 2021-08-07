d38fb3c6593b89b77021a4328d7a3b946f234aa7
Oliver Portee <oliver.portee@gmail.com>
2021-08-07 21:33:12 +0200

    add strength session

---
+++ sport-log-client/frontend/lib/models/strength/strength_session.dart
@@ -0,0 +1,51 @@
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:sport_log/models/movement/movement.dart';
+
+part 'strength_session.g.dart';
+
+@JsonSerializable()
+class StrengthSession {
+  StrengthSession({
+    required this.id,
+    required this.userId,
+    required this.datetime,
+    required this.movementId,
+    required this.movementUnit,
+    required this.interval,
+    required this.comments,
+  });
+
+  int id;
+  int userId;
+  DateTime datetime;
+  int movementId;
+  MovementUnit movementUnit;
+  int? interval;
+  String? comments;
+
+  factory StrengthSession.fromJson(Map<String, dynamic> json) => _$StrengthSessionFromJson(json);
+  Map<String, dynamic> toJson() => _$StrengthSessionToJson(this);
+}
+
+@JsonSerializable()
+class NewStrengthSession {
+  NewStrengthSession({
+    required this.userId,
+    required this.datetime,
+    required this.movementId,
+    required this.movementUnit,
+    required this.interval,
+    required this.comments,
+  });
+
+  int userId;
+  DateTime datetime;
+  int movementId;
+  MovementUnit movementUnit;
+  int? interval;
+  String? comments;
+
+  factory NewStrengthSession.fromJson(Map<String, dynamic> json) => _$NewStrengthSessionFromJson(json);
+  Map<String, dynamic> toJson() => _$NewStrengthSessionToJson(this);
+}
---
+++ sport-log-client/frontend/lib/models/strength/strength_session.g.dart
@@ -0,0 +1,85 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'strength_session.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+StrengthSession _$StrengthSessionFromJson(Map<String, dynamic> json) =>
+    StrengthSession(
+      id: json['id'] as int,
+      userId: json['user_id'] as int,
+      datetime: DateTime.parse(json['datetime'] as String),
+      movementId: json['movement_id'] as int,
+      movementUnit: _$enumDecode(_$MovementUnitEnumMap, json['movement_unit']),
+      interval: json['interval'] as int?,
+      comments: json['comments'] as String?,
+    );
+
+Map<String, dynamic> _$StrengthSessionToJson(StrengthSession instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'user_id': instance.userId,
+      'datetime': instance.datetime.toIso8601String(),
+      'movement_id': instance.movementId,
+      'movement_unit': _$MovementUnitEnumMap[instance.movementUnit],
+      'interval': instance.interval,
+      'comments': instance.comments,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$MovementUnitEnumMap = {
+  MovementUnit.reps: 'Reps',
+  MovementUnit.cal: 'Cal',
+  MovementUnit.meter: 'Meter',
+  MovementUnit.km: 'Km',
+  MovementUnit.yard: 'Yard',
+  MovementUnit.foot: 'Foot',
+  MovementUnit.mile: 'Mile',
+};
+
+NewStrengthSession _$NewStrengthSessionFromJson(Map<String, dynamic> json) =>
+    NewStrengthSession(
+      userId: json['user_id'] as int,
+      datetime: DateTime.parse(json['datetime'] as String),
+      movementId: json['movement_id'] as int,
+      movementUnit: _$enumDecode(_$MovementUnitEnumMap, json['movement_unit']),
+      interval: json['interval'] as int?,
+      comments: json['comments'] as String?,
+    );
+
+Map<String, dynamic> _$NewStrengthSessionToJson(NewStrengthSession instance) =>
+    <String, dynamic>{
+      'user_id': instance.userId,
+      'datetime': instance.datetime.toIso8601String(),
+      'movement_id': instance.movementId,
+      'movement_unit': _$MovementUnitEnumMap[instance.movementUnit],
+      'interval': instance.interval,
+      'comments': instance.comments,
+    };
