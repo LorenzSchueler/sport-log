dd5a069156baedabd8ae635052d3480583b91434
Oliver Portee <oliver.portee@gmail.com>
2021-08-08 11:07:14 +0200

    add action type

---
+++ sport-log-client/frontend/lib/models/action/action.dart
@@ -0,0 +1,46 @@
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'action.g.dart';
+
+@JsonSerializable()
+class Action {
+  Action({
+    required this.id,
+    required this.name,
+    required this.actionProviderId,
+    required this.description,
+    required this.createBefore,
+    required this.deleteAfter,
+  });
+
+  int id;
+  String name;
+  int actionProviderId;
+  String? description;
+  int createBefore;
+  int deleteAfter;
+
+  factory Action.fromJson(Map<String, dynamic> json) => _$ActionFromJson(json);
+  Map<String, dynamic> toJson() => _$ActionToJson(this);
+}
+
+@JsonSerializable()
+class NewAction {
+  NewAction({
+    required this.name,
+    required this.actionProviderId,
+    required this.description,
+    required this.createBefore,
+    required this.deleteAfter,
+  });
+
+  String name;
+  int actionProviderId;
+  String? description;
+  int createBefore;
+  int deleteAfter;
+
+  factory NewAction.fromJson(Map<String, dynamic> json) => _$NewActionFromJson(json);
+  Map<String, dynamic> toJson() => _$NewActionToJson(this);
+}
---
+++ sport-log-client/frontend/lib/models/action/action.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'action.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Action _$ActionFromJson(Map<String, dynamic> json) => Action(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      actionProviderId: json['action_provider_id'] as int,
+      description: json['description'] as String?,
+      createBefore: json['create_before'] as int,
+      deleteAfter: json['delete_after'] as int,
+    );
+
+Map<String, dynamic> _$ActionToJson(Action instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'action_provider_id': instance.actionProviderId,
+      'description': instance.description,
+      'create_before': instance.createBefore,
+      'delete_after': instance.deleteAfter,
+    };
+
+NewAction _$NewActionFromJson(Map<String, dynamic> json) => NewAction(
+      name: json['name'] as String,
+      actionProviderId: json['action_provider_id'] as int,
+      description: json['description'] as String?,
+      createBefore: json['create_before'] as int,
+      deleteAfter: json['delete_after'] as int,
+    );
+
+Map<String, dynamic> _$NewActionToJson(NewAction instance) => <String, dynamic>{
+      'name': instance.name,
+      'action_provider_id': instance.actionProviderId,
+      'description': instance.description,
+      'create_before': instance.createBefore,
+      'delete_after': instance.deleteAfter,
+    };
