29b8f2be4a860b0fd0330f1f4ce44657717df686
Lorenz Schüler <lorenz@saarow.de>
2021-08-08 12:45:38 +0200

    add trigger and deleted and last_change to user

--- sport-log-types/migrations/2021-06-27-085316_user/down.sql
+++ sport-log-types/migrations/2021-06-27-085316_user/down.sql
@@ -1,1 +1,2 @@
 drop table "user";
+drop function trigger_set_timestamp;
--- sport-log-types/migrations/2021-06-27-085316_user/up.sql
+++ sport-log-types/migrations/2021-06-27-085316_user/up.sql
@@ -1,11 +1,24 @@
+create or replace function trigger_set_timestamp()
+returns trigger as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$ language plpgsql;
+
 create table "user" (
-    id serial primary key,
+    id bigint primary key,
     username varchar(80) not null check (length(username) >= 2),
     password char(96) not null,
     email varchar(80) not null,
+    last_change timestamptz not null default now(),
+    deleted boolean not null default false,
     unique (username),
     unique (email)
 );
+create trigger set_timestamp before update on "user"
+    for each row execute procedure trigger_set_timestamp();
+
 create index on "user" (username);
 
 insert into "user" (username, password, email) values 
