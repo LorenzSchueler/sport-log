4336cd6dbc6a32cab34e1ffeec222993e4907c7e
Oliver Portee <oliver.portee@gmail.com>
2021-08-08 09:44:37 +0200

    add user type

---
+++ sport-log-client/frontend/lib/models/user/user.dart
@@ -0,0 +1,22 @@
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  User({
+    required this.id,
+    required this.username,
+    required this.password,
+    required this.email,
+  });
+
+  int id;
+  String username;
+  String password;
+  String email;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
---
+++ sport-log-client/frontend/lib/models/user/user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as int,
+      username: json['username'] as String,
+      password: json['password'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'password': instance.password,
+      'email': instance.email,
+    };
