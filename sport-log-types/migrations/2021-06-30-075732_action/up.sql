8cb0ca91e66975763b3efe2e9b77973abb15c963
Lorenz Schüler <lorenz@saarow.de>
2021-08-09 14:58:00 +0200

    add multiple actions for wodify-wod

--- sport-log-types/migrations/2021-06-30-075732_action/up.sql
+++ sport-log-types/migrations/2021-06-30-075732_action/up.sql
@@ -12,7 +12,7 @@ insert into action_provider (name, password, platform_id, description) values
     ('wodify-login', '$argon2id$v=19$m=4096,t=3,p=1$NZeOJg1K37UlxV5wB7yFhg$C7HNfVK9yLZTJyvJNSOhvYRfUK+nGo1rz0lIck1aO6c', 1, 
         'Wodify Login can reserve spots in classes. The action names correspond to the class types.'), -- "wodify-login-passwd"
     ('wodify-wod', '$argon2id$v=19$m=4096,t=3,p=1$FscunZHcMdL3To4Zxc5z5w$InsqwdstEFdkszaokG1rk0HS0oazMm4zTynD6pjQEgw', 1,  
-        'Wodify Wod can fetch the Workout of the Day and save it in your wods.'), -- "wodify-wod-passwd"
+        'Wodify Wod can fetch the Workout of the Day and save it in your wods. The action names correspond to the class type the wod should be fetched for.'), -- "wodify-wod-passwd"
     ('sportstracker-fetch', '$argon2id$v=19$m=4096,t=3,p=1$mmRowryKPKBhRSvrRZRFmg$VPInpHpMq47ZEymwSojrst+CWVOoHopBlvSIwybchAg', 2,  
         'Sportstracker Fetch can fetch the latests workouts recorded with sportstracker and save them in your cardio sessions.'); -- "sportstracker-fetch-passwd"
 
@@ -26,10 +26,12 @@ create table action (
     unique (action_provider_id, name)
 );
 insert into action (name, action_provider_id, description, create_before, delete_after) values 
-    ('Crossfit', 1, 'Reserve a spot in a CrossFit class.', 168, 0), 
+    ('CrossFit', 1, 'Reserve a spot in a CrossFit class.', 168, 0), 
     ('Weightlifting', 1, 'Reserve a spot in a Weightlifting class.', 168, 0), 
     ('Open Fridge', 1, 'Reserve a spot in a Open Fridge class.', 168, 0),
-    ('fetch', 2, 'Fetch and save the wod for the current day.', 168, 24),
+    ('CrossFit', 2, 'Fetch and save the CrossFit wod for the current day.', 168, 0), 
+    ('Weightlifting', 2, 'Fetch and save the Weightlifting wod for the current day.', 168, 0), 
+    ('Open Fridge', 2, 'Fetch and save the Open Fridge wod for the current day.', 168, 0), 
     ('fetch', 3, 'Fetch and save new workouts.', 168, 0);
 
 create table action_rule (
@@ -71,9 +73,9 @@ insert into action_event (user_id, action_id, datetime, enabled) values
     (2, 2, '2021-07-04 19:00:00', false),
     (1, 4, '2021-08-29 00:00:00', true), 
     (1, 4, '2021-08-30 00:00:00', true), 
-    (1, 5, '2021-07-01 09:00:00', true), 
-    (1, 5, '2021-07-01 10:00:00', true), 
-    (1, 5, '2021-07-01 11:00:00', true),
-    (3, 5, '2021-07-01 09:00:00', true), 
-    (3, 5, '2021-07-01 10:00:00', true), 
-    (3, 5, '2021-07-01 11:00:00', true); 
+    (1, 7, '2021-07-01 09:00:00', true), 
+    (1, 7, '2021-07-01 10:00:00', true), 
+    (1, 7, '2021-07-01 11:00:00', true),
+    (3, 7, '2021-07-01 09:00:00', true), 
+    (3, 7, '2021-07-01 10:00:00', true), 
+    (3, 7, '2021-07-01 11:00:00', true); 
