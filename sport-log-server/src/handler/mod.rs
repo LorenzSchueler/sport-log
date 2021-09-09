9ed4c0591bc8b18f58aaafb74f66cba9c89931f5
Lorenz Schüler <lorenz@saarow.de>
2021-09-09 16:34:10 +0200

    add JsonResult with status and message

--- sport-log-server/src/handler/mod.rs
+++ sport-log-server/src/handler/mod.rs
@@ -1,11 +1,18 @@
-use std::ops::Deref;
+use std::{io::Cursor, ops::Deref};
 
 use chrono::{DateTime, NaiveTime, Utc};
 use diesel::{
     result::{DatabaseErrorKind as DbError, Error as DieselError},
     QueryResult,
 };
-use rocket::{http::Status, request::FromParam, serde::json::Json};
+use rocket::{
+    http::{ContentType, Status},
+    request::FromParam,
+    response::Responder,
+    serde::json::Json,
+    Request, Response,
+};
+use serde::{Deserialize, Serialize};
 use tracing::warn;
 
 pub mod account;
@@ -19,23 +26,66 @@ pub mod platform;
 pub mod strength;
 pub mod user;
 
+pub struct JsonError {
+    pub status: Status,
+    pub message: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SerializableJsonError {
+    pub status: u16,
+    pub message: Option<String>,
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for JsonError {
+    fn respond_to(self, _request: &'r Request<'_>) -> Result<Response<'o>, Status> {
+        let json = serde_json::to_string(&SerializableJsonError {
+            status: self.status.code,
+            message: self.message,
+        })
+        .map_err(|_| Status::InternalServerError)?;
+        Ok(Response::build()
+            .status(self.status)
+            .header(ContentType::JSON)
+            .sized_body(json.len(), Cursor::new(json))
+            .finalize())
+    }
+}
+
+pub type JsonResult<T> = Result<Json<T>, JsonError>;
+
 trait IntoJson<T> {
-    fn into_json(self) -> Result<Json<T>, Status>;
+    fn into_json(self) -> JsonResult<T>;
 }
 
 impl<T> IntoJson<T> for QueryResult<T> {
-    fn into_json(self) -> Result<Json<T>, Status> {
+    fn into_json(self) -> JsonResult<T> {
         self.map(Json).map_err(|diesel_error| match diesel_error {
-            DieselError::NotFound => Status::NoContent,
-            DieselError::DatabaseError(ref db_error, ref _db_error_info) => match db_error {
-                DbError::UniqueViolation => Status::Conflict,
-                DbError::ForeignKeyViolation => Status::Conflict,
+            DieselError::NotFound => JsonError {
+                status: Status::NoContent,
+                message: None,
+            },
+            DieselError::DatabaseError(ref db_error, ref db_error_info) => match db_error {
+                DbError::UniqueViolation => JsonError {
+                    status: Status::Conflict,
+                    message: Some(db_error_info.message().to_owned()),
+                },
+                DbError::ForeignKeyViolation => JsonError {
+                    status: Status::Conflict,
+                    message: Some(db_error_info.message().to_owned()),
+                },
                 _ => {
                     warn!("{:?}", diesel_error);
-                    Status::InternalServerError
+                    JsonError {
+                        status: Status::InternalServerError,
+                        message: None,
+                    }
                 }
             },
-            _ => Status::InternalServerError,
+            _ => JsonError {
+                status: Status::InternalServerError,
+                message: None,
+            },
         })
     }
 }
