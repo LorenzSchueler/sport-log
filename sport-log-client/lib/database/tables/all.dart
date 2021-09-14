0171477c0503b616aeed152e77edd48206acdc9c
Oliver Portee <oliver.portee@gmail.com>
2021-09-14 23:46:09 +0200

    rename tables names to *_table{s}.dart

--- sport-log-client/lib/database/tables/all.dart
+++ sport-log-client/lib/database/tables/all.dart
@@ -1,9 +1,9 @@
 
-export 'action.dart';
-export 'cardio.dart';
-export 'diary.dart';
-export 'metcon.dart';
-export 'movement.dart';
-export 'platform.dart';
-export 'strength.dart';
-export 'wod.dart';
+export 'action_tables.dart';
+export 'cardio_tables.dart';
+export 'diary_table.dart';
+export 'metcon_tables.dart';
+export 'movement_table.dart';
+export 'platform_tables.dart';
+export 'strength_tables.dart';
+export 'wod_table.dart';
