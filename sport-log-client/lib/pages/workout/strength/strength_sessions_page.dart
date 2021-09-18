428ccb394a27d9f43a6a92cf3cdf350fdb708f99
Oliver Portee <oliver.portee@gmail.com>
2021-09-18 12:11:36 +0200

    remove implicitly animated list

--- sport-log-client/lib/pages/workout/strength/strength_sessions_page.dart
+++ sport-log-client/lib/pages/workout/strength/strength_sessions_page.dart
@@ -1,15 +1,11 @@
 import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
-import 'package:implicitly_animated_reorderable_list/transitions.dart';
 import 'package:intl/intl.dart';
 import 'package:sport_log/api/api.dart';
 import 'package:sport_log/data_provider/data_providers/strength_data_provider.dart';
 import 'package:sport_log/helpers/human_readable.dart';
 import 'package:sport_log/helpers/logger.dart';
-import 'package:sport_log/helpers/state/local_state.dart';
-import 'package:sport_log/helpers/state/page_return.dart';
 import 'package:sport_log/models/all.dart';
 import 'package:sport_log/models/strength/all.dart';
 
@@ -18,8 +14,7 @@ class StrengthSessionsPage extends StatefulWidget {
     Key? key,
     required this.start,
     required this.end,
-  })  : assert(
-            (start != null && end != null) || (start == null && end == null)),
+  })  : assert((start == null) == (end == null)),
         super(key: key);
 
   final DateTime? start;
@@ -31,8 +26,10 @@ class StrengthSessionsPage extends StatefulWidget {
 
 class _StrengthSessionsPageState extends State<StrengthSessionsPage> {
   final _dataProvider = StrengthDataProvider();
-  LocalState<StrengthSessionDescription> _state = LocalState.empty();
+
   final _logger = Logger('StrengthSessionsPage');
+
+  List<StrengthSessionDescription> _ssds = [];
 
   @override
   void initState() {
@@ -40,29 +37,19 @@ class _StrengthSessionsPageState extends State<StrengthSessionsPage> {
     update();
   }
 
+  static int byDate(
+          StrengthSessionDescription ssd1, StrengthSessionDescription ssd2) =>
+      ssd2.strengthSession.datetime.compareTo(ssd1.strengthSession.datetime);
+
   void update() async {
     _logger.d(
         'Updating strength sessions with start = ${widget.start}, end = ${widget.end}');
     _dataProvider
         .filterDescriptions(from: widget.start, until: widget.end)
         .then((ssds) {
-      setState(() => _state = LocalState.fromList(ssds));
+      ssds.sort(byDate);
+      setState(() => _ssds = ssds);
     });
-  }
-
-  void _handlePageReturn(dynamic object) {
-    if (object is ReturnObject<StrengthSessionDescription>) {
-      switch (object.action) {
-        case ReturnAction.created:
-          setState(() => _state.create(object.object));
-          break;
-        case ReturnAction.updated:
-          setState(() => _state.update(object.object));
-          break;
-        case ReturnAction.deleted:
-          setState(() => _state.delete(object.object.id));
-      }
-    }
   }
 
   // full update (from server)
@@ -95,21 +82,19 @@ class _StrengthSessionsPageState extends State<StrengthSessionsPage> {
   }
 
   Widget _buildStrengthSessionList(BuildContext context) {
-    if (_state.isEmpty) {
+    if (_ssds.isEmpty) {
       return const Center(child: Text('No strength sessions there.'));
     }
     return Scrollbar(
-      child: ImplicitlyAnimatedList(
-        items: _state.sortedBy((o1, o2) =>
-            -o1.strengthSession.datetime.compareTo(o2.strengthSession.datetime)),
-        itemBuilder: _buildStrengthSession,
-        areItemsTheSame: StrengthSessionDescription.areTheSame,
+      child: ListView.builder(
+        itemBuilder: _strengthSessionBuilder,
+        itemCount: _ssds.length,
       ),
     );
   }
 
-  Widget _buildStrengthSession(BuildContext context,
-      Animation<double> animation, StrengthSessionDescription ssd, int i) {
+  Widget _strengthSessionBuilder(BuildContext context, int index) {
+    final ssd = _ssds[index];
     final String date =
         DateFormat('dd.MM.yyyy').format(ssd.strengthSession.datetime);
     final String time =
@@ -125,55 +110,49 @@ class _StrengthSessionsPageState extends State<StrengthSessionsPage> {
     final String text =
         ssd.strengthSets?.map((ss) => ss.toDisplayName()).join(', ') ?? '';
 
-    return SizeFadeTransition(
-      key: ValueKey(ssd.strengthSession.id),
-      animation: animation,
-      curve: Curves.easeInOut,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-        child: ExpansionTileCard(
-          leading: CircleAvatar(child: Text(ssd.movement.name[0])),
-          title: Text(title),
-          subtitle: Text(subtitle),
-          children: [
-            const Divider(),
-            ssd.strengthSets == null
-                ? const CircularProgressIndicator()
-                : Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                    child: Text(text),
-                  ),
-            const Divider(),
-            if (ssd.strengthSession.comments != null)
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                child: Text(ssd.strengthSession.comments!),
-              ),
-            if (ssd.strengthSession.comments != null) const Divider(),
-            ButtonBar(
-              alignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: () {}, // TODO
-                  icon: const Icon(Icons.delete),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+      child: ExpansionTileCard(
+        leading: CircleAvatar(child: Text(ssd.movement.name[0])),
+        title: Text(title),
+        subtitle: Text(subtitle),
+        children: [
+          const Divider(),
+          ssd.strengthSets == null
+              ? const CircularProgressIndicator()
+              : Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  child: Text(text),
                 ),
-                IconButton(
-                  onPressed: () {}, // TODO
-                  icon: const Icon(Icons.edit),
-                ),
-              ],
+          const Divider(),
+          if (ssd.strengthSession.comments != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Text(ssd.strengthSession.comments!),
             ),
-          ],
-          onExpansionChanged: (expanded) async {
-            if (expanded && ssd.strengthSets == null) {
-              ssd.strengthSets =
-                  await _dataProvider.getStrengthSetsByStrengthSession(ssd.id);
-              setState(() {});
-            }
-          },
-        ),
+          if (ssd.strengthSession.comments != null) const Divider(),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {}, // TODO
+                icon: const Icon(Icons.delete),
+              ),
+              IconButton(
+                onPressed: () {}, // TODO
+                icon: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+        ],
+        onExpansionChanged: (expanded) async {
+          if (expanded && ssd.strengthSets == null) {
+            ssd.strengthSets =
+                await _dataProvider.getStrengthSetsByStrengthSession(ssd.id);
+            setState(() {});
+          }
+        },
       ),
     );
   }
