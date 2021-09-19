d733660e31e897031c2129f3e72b71ee2ce03b16
Lorenz Schüler <lorenz@saarow.de>
2021-09-19 11:08:18 +0200

    add new types to AccountData for sync

--- sport-log-types/src/types/account.rs
+++ sport-log-types/src/types/account.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    Action, ActionEvent, ActionProvider, ActionRule, CardioSession, Diary, Metcon, MetconMovement,
-    MetconSession, Movement, Platform, PlatformCredential, Route, StrengthSession, StrengthSet,
-    User, Wod,
+    Action, ActionEvent, ActionProvider, ActionRule, CardioBlueprint, CardioSession, Diary, Metcon,
+    MetconItem, MetconMovement, MetconSession, Movement, Platform, PlatformCredential, Route,
+    StrengthBlueprint, StrengthBlueprintSet, StrengthSession, StrengthSet, TrainingPlan, User, Wod,
 };
 
 /// A representation of all or recentily updated data belonging to a user account.
@@ -15,13 +15,18 @@ pub struct AccountData {
     pub diaries: Vec<Diary>,
     pub wods: Vec<Wod>,
     pub movements: Vec<Movement>,
+    pub strength_blueprints: Vec<StrengthBlueprint>,
+    pub strength_blueprint_sets: Vec<StrengthBlueprintSet>,
     pub strength_sessions: Vec<StrengthSession>,
     pub strength_sets: Vec<StrengthSet>,
     pub metcons: Vec<Metcon>,
     pub metcon_sessions: Vec<MetconSession>,
     pub metcon_movements: Vec<MetconMovement>,
+    pub metcon_items: Vec<MetconItem>,
+    pub cardio_blueprints: Vec<CardioBlueprint>,
     pub cardio_sessions: Vec<CardioSession>,
     pub routes: Vec<Route>,
+    pub training_plans: Vec<TrainingPlan>,
     pub platforms: Vec<Platform>,
     pub platform_credentials: Vec<PlatformCredential>,
     pub action_providers: Vec<ActionProvider>,
