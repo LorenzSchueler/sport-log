1b2c8d977e95706216a504e77239fddf15ecee57
Lorenz Schüler <lorenz@saarow.de>
2021-09-27 20:42:07 +0200

    add mapbox map

--- sport-log-client/lib/pages/workout/cardio_sessions/overview_page.dart
+++ sport-log-client/lib/pages/workout/cardio_sessions/overview_page.dart
@@ -1,5 +1,8 @@
+import 'package:location/location.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:flutter/material.dart';
 import 'package:sport_log/helpers/logger.dart';
+import 'package:sport_log/helpers/secrets.dart';
 
 class CardioSessionsPage extends StatefulWidget {
   const CardioSessionsPage({Key? key}) : super(key: key);
@@ -13,10 +16,30 @@ class CardioSessionsPageState extends State<CardioSessionsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Not implemented yet :('));
+    return MapScreen();
   }
 
   void onFabTapped(BuildContext context) {
     _logger.d('FAB tapped!');
+  }
+}
+
+class MapScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String token = Secrets.mapboxAccessToken;
+    final String style = 'mapbox://styles/mapbox/outdoors-v11';
+
+    return Scaffold(
+      body: MapboxMap(
+        accessToken: token,
+        styleString: style,
+        initialCameraPosition: CameraPosition(
+          zoom: 12.0,
+          target: LatLng(47.27, 11.33),
+        ),
+        compassEnabled: true,
+      ),
+    );
   }
 }
