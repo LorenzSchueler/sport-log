9023298a408feb2e69353bf29ba6e03a81117ef2
Lorenz Schüler <lorenz@saarow.de>
2021-09-28 14:15:09 +0200

    show map, points, line and symbol; mark current location

--- sport-log-client/lib/pages/workout/cardio_sessions/overview_page.dart
+++ sport-log-client/lib/pages/workout/cardio_sessions/overview_page.dart
@@ -13,10 +13,81 @@ class CardioSessionsPage extends StatefulWidget {
 
 class CardioSessionsPageState extends State<CardioSessionsPage> {
   final _logger = Logger('CardioSessionsPage');
+  final String token = Secrets.mapboxAccessToken;
+  final String style = 'mapbox://styles/mapbox/outdoors-v11';
+  late MapboxMapController mapController;
+
+  void addSymbol(MapboxMapController controller) async {
+    var location = await acquireCurrentLocation();
+    //var location = LatLng(47.28, 11.33);
+
+    await controller.animateCamera(
+      CameraUpdate.newLatLng(location),
+    );
+
+    await controller.addCircle(
+      CircleOptions(
+        circleRadius: 8.0,
+        circleColor: '#006992',
+        circleOpacity: 0.8,
+        geometry: location,
+        draggable: false,
+      ),
+    );
+
+    await controller.addLine(const LineOptions(
+        lineColor: "red",
+        lineBlur: 1.0,
+        lineWidth: 5,
+        geometry: [
+          LatLng(47.28, 11.33),
+          LatLng(47.29, 11.33),
+          LatLng(47.29, 11.34)
+        ]));
+    await controller.addSymbol(const SymbolOptions(
+        iconImage: "town-hall-15", geometry: LatLng(47.29, 11.34)));
+  }
+
+  void markLocation(MapboxMapController controller, LatLng location) async {
+    await controller.addCircle(
+      CircleOptions(
+        circleRadius: 8.0,
+        circleColor: '#006992',
+        circleOpacity: 0.8,
+        geometry: location,
+        draggable: false,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MapScreen();
+    return Scaffold(
+      body: MapboxMap(
+        accessToken: token,
+        styleString: style,
+        initialCameraPosition: const CameraPosition(
+          zoom: 12.0,
+          target: LatLng(47.27, 11.33),
+        ),
+        compassEnabled: true,
+        compassViewPosition: CompassViewPosition.TopRight,
+        myLocationEnabled: true,
+        myLocationRenderMode: MyLocationRenderMode.GPS,
+        myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+        onMapCreated: (MapboxMapController controller) =>
+            mapController = controller,
+        onStyleLoadedCallback: () => addSymbol(mapController),
+        onMapLongClick: (point, LatLng coordinates) =>
+            markLocation(mapController, coordinates),
+        onUserLocationUpdated: (UserLocation location) {
+          print(location.altitude);
+          print(location.position.latitude);
+          print(location.position.longitude);
+        },
+      ),
+    );
+    ;
   }
 
   void onFabTapped(BuildContext context) {
@@ -24,22 +95,28 @@ class CardioSessionsPageState extends State<CardioSessionsPage> {
   }
 }
 
-class MapScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final String token = Secrets.mapboxAccessToken;
-    final String style = 'mapbox://styles/mapbox/outdoors-v11';
+Future<LatLng?> acquireCurrentLocation() async {
+  Location location = Location();
 
-    return Scaffold(
-      body: MapboxMap(
-        accessToken: token,
-        styleString: style,
-        initialCameraPosition: CameraPosition(
-          zoom: 12.0,
-          target: LatLng(47.27, 11.33),
-        ),
-        compassEnabled: true,
-      ),
-    );
+  bool serviceEnabled = await location.serviceEnabled();
+  if (!serviceEnabled) {
+    serviceEnabled = await location.requestService();
+    if (!serviceEnabled) {
+      return null;
+    }
   }
+
+  PermissionStatus permissionGranted = await location.hasPermission();
+  if (permissionGranted == PermissionStatus.denied) {
+    permissionGranted = await location.requestPermission();
+    if (permissionGranted != PermissionStatus.granted) {
+      return null;
+    }
+  }
+
+  //location.enableBackgroundMode(enable: true);
+  //location.onLocationChanged.listen((LocationData currentLocation) {});
+
+  final locationData = await location.getLocation();
+  return LatLng(locationData.latitude, locationData.longitude);
 }
