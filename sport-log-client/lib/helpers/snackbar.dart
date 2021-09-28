4eb169096272816eed4ef1c922f0461848fe753a
Oliver Portee <oliver.portee@gmail.com>
2021-09-28 20:10:13 +0200

    add method to show simple toast

--- sport-log-client/lib/helpers/snackbar.dart
+++ sport-log-client/lib/helpers/snackbar.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:sport_log/helpers/theme.dart';
 
 void showSimpleSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+}
+
+void showSimpleToast(BuildContext context, String text) {
+  Fluttertoast.showToast(
+    msg: text,
+    backgroundColor: onBackgroundColorOf(context),
+    textColor: backgroundColorOf(context),
+  );
 }
