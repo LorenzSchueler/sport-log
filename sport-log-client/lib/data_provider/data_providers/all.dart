43fc5801f09d018bf5df42d68d89ae5f9c3cdcb3
Oliver Portee <oliver.portee@gmail.com>
2021-09-28 12:06:21 +0200

    add all.dart for data providers

---
+++ sport-log-client/lib/data_provider/data_providers/all.dart
@@ -0,0 +1,8 @@
+export 'action_event_data_provider.dart';
+export 'action_rule_data_provider.dart';
+export 'diary_data_provider.dart';
+export 'metcon_data_provider.dart';
+export 'movement_data_provider.dart';
+export 'platform_credential_data_provider.dart';
+export 'strength_data_provider.dart';
+export 'wod_data_provider.dart';
