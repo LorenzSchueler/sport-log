eaff4d1304ed54f25205239b2201963d2d0d6586
Lorenz Schüler <lorenz@saarow.de>
2021-09-29 14:39:57 +0200

    add gpx tracking and draw path

--- sport-log-client/lib/pages/cardio/tracking_page.dart
+++ sport-log-client/lib/pages/cardio/tracking_page.dart
@@ -5,6 +5,8 @@ import 'package:sport_log/helpers/logger.dart';
 import 'package:sport_log/helpers/secrets.dart';
 import 'package:sport_log/helpers/theme.dart';
 
+enum TrackingMode { Tracking, Paused, NotStarted }
+
 class CardioTrackingPage extends StatefulWidget {
   const CardioTrackingPage({Key? key}) : super(key: key);
 
@@ -20,17 +22,48 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
 
   List<LatLng> locations = [];
   late Line line;
-  LineOptions lineoptions =
-      const LineOptions(lineColor: "red", lineWidth: 3, geometry: []);
+
+  TrackingMode trackingMode = TrackingMode.NotStarted;
+
+  String locationInfo = "";
 
   late MapboxMapController mapController;
 
+  Future<LatLng?> acquireCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    location.changeSettings(accuracy: LocationAccuracy.high);
+
+    //location.enableBackgroundMode(enable: true);
+    location.onLocationChanged.listen(
+        (LocationData currentLocation) => locationConsumer(currentLocation));
+
+    final locationData = await location.getLocation();
+    return LatLng(locationData.latitude, locationData.longitude);
+  }
+
   void init(MapboxMapController controller) async {
-    await controller.addLine(lineoptions);
+    await controller.addLine(
+        const LineOptions(lineColor: "red", lineWidth: 3, geometry: []));
     line = controller.lines.first;
 
     var location = await acquireCurrentLocation();
-    //var location = LatLng(47.28, 11.33);
 
     await controller.animateCamera(
       CameraUpdate.newLatLng(location),
@@ -40,26 +73,46 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
       CircleOptions(
         circleRadius: 8.0,
         circleColor: '#0060a0',
-        circleOpacity: 0.8,
+        circleOpacity: 0.5,
+        geometry: location,
+        draggable: false,
+      ),
+    );
+    await controller.addCircle(
+      CircleOptions(
+        circleRadius: 20.0,
+        circleColor: '#0060a0',
+        circleOpacity: 0.3,
         geometry: location,
         draggable: false,
       ),
     );
 
-    //await controller.addSymbol(const SymbolOptions(
-    //iconImage: "town-hall-15", geometry: LatLng(47.29, 11.34)));
-
-    _logger.i("requesting location");
-    var x = await controller.requestMyLocationLatLng();
-    _logger.i("requested location: ${x}");
+    //_logger.i("requesting location");
+    //var mapboxLocation = await controller.requestMyLocationLatLng();
+    //_logger.i("requested location: $mapboxLocation");
   }
 
-  void markPosition(MapboxMapController controller, LatLng location) async {
+  void locationConsumer(LocationData location) {
+    setState(() {
+      locationInfo = """location provider: ${location.provider}
+accuracy: ${location.accuracy}
+time: ${location.time}""";
+    });
+
+    _logger.i(locationInfo);
+
+    if (trackingMode == TrackingMode.Tracking) {
+      extendLine(mapController, LatLng(location.latitude, location.longitude));
+    }
+  }
+
+  void markLatLng(MapboxMapController controller, LatLng location) async {
     await controller.addCircle(
       CircleOptions(
         circleRadius: 8.0,
         circleColor: '#008080',
-        circleOpacity: 0.8,
+        circleOpacity: 0.5,
         geometry: location,
         draggable: false,
       ),
@@ -74,7 +127,7 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
   Widget _buildCard(String title, String subtitle) {
     return Card(
       child: ListTile(
-        contentPadding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+        contentPadding: const EdgeInsets.only(top: 2),
         title: Text(
           title,
           textAlign: TextAlign.center,
@@ -89,9 +142,74 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
     );
   }
 
+  List<Widget> _buildButtons() {
+    if (trackingMode == TrackingMode.Tracking) {
+      return [
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red[400]),
+                onPressed: () => setState(() {
+                      trackingMode = TrackingMode.Paused;
+                    }),
+                child: const Text("pause"))),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red[400]),
+                onPressed: () => setState(() {
+                      trackingMode = TrackingMode.NotStarted;
+                    }),
+                child: const Text("stop"))),
+      ];
+    } else if (trackingMode == TrackingMode.Paused) {
+      return [
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.green[400]),
+                onPressed: () => setState(() {
+                      trackingMode = TrackingMode.Tracking;
+                    }),
+                child: const Text("continue"))),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red[400]),
+                onPressed: () => setState(() {
+                      trackingMode = TrackingMode.NotStarted;
+                    }),
+                child: const Text("stop"))),
+      ];
+    } else {
+      return [
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.green[400]),
+                onPressed: () => setState(() {
+                      trackingMode = TrackingMode.Tracking;
+                    }),
+                child: const Text("start"))),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red[400]),
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("cancel"))),
+      ];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
+      Card(
+          margin: const EdgeInsets.only(top: 25, bottom: 5),
+          child: Text(locationInfo)),
       Expanded(
           child: MapboxMap(
         accessToken: token,
@@ -102,19 +220,20 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
         ),
         compassEnabled: true,
         compassViewPosition: CompassViewPosition.TopRight,
-        myLocationEnabled: true,
-        myLocationRenderMode: MyLocationRenderMode.GPS,
-        myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+        //myLocationEnabled: true,
+        //myLocationRenderMode: MyLocationRenderMode.NORMAL,
+        //myLocationTrackingMode: MyLocationTrackingMode.Tracking,
         onMapCreated: (MapboxMapController controller) =>
             mapController = controller,
         onStyleLoadedCallback: () => init(mapController),
         onMapClick: (point, LatLng coordinates) =>
-            markPosition(mapController, coordinates),
+            markLatLng(mapController, coordinates),
         onMapLongClick: (point, LatLng coordinates) =>
             extendLine(mapController, coordinates),
         onUserLocationUpdated: (UserLocation location) {
           _logger.i(
               "position ${location.position}; elevation ${location.altitude}");
+          //extendLine(mapController, location.position);
         },
       )),
       Container(
@@ -140,49 +259,8 @@ class CardioTrackingPageState extends State<CardioTrackingPage> {
           color: onPrimaryColorOf(context),
           padding: const EdgeInsets.all(5),
           child: Row(
-            children: [
-              Expanded(
-                  child: ElevatedButton(
-                      style:
-                          ElevatedButton.styleFrom(primary: Colors.green[400]),
-                      onPressed: null,
-                      child: const Text("start"))),
-              const SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(primary: Colors.red[400]),
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: const Text("cancel"))),
-            ],
+            children: _buildButtons(),
           ))
     ]);
   }
-}
-
-Future<LatLng?> acquireCurrentLocation() async {
-  Location location = Location();
-
-  bool serviceEnabled = await location.serviceEnabled();
-  if (!serviceEnabled) {
-    serviceEnabled = await location.requestService();
-    if (!serviceEnabled) {
-      return null;
-    }
-  }
-
-  PermissionStatus permissionGranted = await location.hasPermission();
-  if (permissionGranted == PermissionStatus.denied) {
-    permissionGranted = await location.requestPermission();
-    if (permissionGranted != PermissionStatus.granted) {
-      return null;
-    }
-  }
-
-  //location.enableBackgroundMode(enable: true);
-  //location.onLocationChanged.listen((LocationData currentLocation) {});
-
-  final locationData = await location.getLocation();
-  return LatLng(locationData.latitude, locationData.longitude);
 }
