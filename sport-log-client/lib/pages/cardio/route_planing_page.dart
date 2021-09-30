5ccaf8a47027f763b35305ab0a95b1650b438613
Lorenz Schüler <lorenz@saarow.de>
2021-09-30 15:03:36 +0200

    drag an drop listelements to switch order & numerate locations on map

--- sport-log-client/lib/pages/cardio/route_planing_page.dart
+++ sport-log-client/lib/pages/cardio/route_planing_page.dart
@@ -19,25 +19,61 @@ class RoutePlanningPageState extends State<RoutePlanningPage> {
 
   List<LatLng> locations = [];
   Line? line;
+  List<Circle> circles = [];
+  List<Symbol> symbols = [];
 
   late MapboxMapController mapController;
 
-  void markLatLng(MapboxMapController controller, LatLng location) async {
-    await controller.addCircle(
+  void markLatLng(
+      MapboxMapController controller, LatLng latLng, int number) async {
+    symbols.add(await controller.addSymbol(SymbolOptions(
+        textField: "$number",
+        textOffset: const Offset(0, 1),
+        geometry: latLng)));
+    circles.add(await controller.addCircle(
       CircleOptions(
         circleRadius: 8.0,
-        circleColor: '#008080',
+        circleColor: '#0060a0',
         circleOpacity: 0.5,
-        geometry: location,
+        geometry: latLng,
         draggable: false,
       ),
-    );
+    ));
   }
 
   void extendLine(MapboxMapController controller, LatLng location) async {
-    locations.add(location);
+    setState(() {
+      locations.add(location);
+    });
+    markLatLng(controller, location, locations.length);
     line ??= await controller.addLine(
         const LineOptions(lineColor: "red", lineWidth: 3, geometry: []));
+    await controller.updateLine(line, LineOptions(geometry: locations));
+  }
+
+  void switchOrder(
+      MapboxMapController controller, int oldIndex, int newIndex) async {
+    setState(() {
+      _logger.i("old: $oldIndex, new: $newIndex");
+      if (oldIndex < newIndex - 1) {
+        LatLng location = locations.removeAt(oldIndex);
+        if (newIndex - 1 == locations.length) {
+          locations.add(location);
+        } else {
+          locations.insert(newIndex - 1, location);
+        }
+      } else if (oldIndex > newIndex) {
+        locations.insert(newIndex, locations.removeAt(oldIndex));
+      }
+    });
+
+    await controller.removeCircles(circles);
+    circles = [];
+    await controller.removeSymbols(symbols);
+    symbols = [];
+    locations.asMap().forEach((index, latLng) {
+      markLatLng(controller, latLng, index + 1);
+    });
     await controller.updateLine(line, LineOptions(geometry: locations));
   }
 
@@ -59,6 +95,77 @@ class RoutePlanningPageState extends State<RoutePlanningPage> {
     );
   }
 
+  Widget _buildDraggableList(List<LatLng> locations) {
+    List<Widget> listElements = [];
+    for (int index = 0; index < locations.length; index++) {
+      listElements.add(
+        DragTarget(
+            onWillAccept: (value) => true,
+            onAccept: (value) =>
+                switchOrder(mapController, value as int, index),
+            builder: (context, candidates, reject) {
+              if (candidates.isNotEmpty) {
+                int index = candidates[0] as int;
+                return ListTile(
+                  trailing: const Icon(
+                    Icons.drag_handle,
+                    color: Colors.red,
+                  ),
+                  title: Text("${index + 1}"),
+                  dense: true,
+                );
+              } else {
+                return const Divider();
+              }
+            }),
+      );
+
+      ListTile listTile = ListTile(
+        trailing: const Icon(
+          Icons.drag_handle,
+        ),
+        title: Text("${index + 1}"),
+        dense: true,
+      );
+
+      listElements.add(
+        Draggable(
+          data: index,
+          child: listTile,
+          childWhenDragging: Opacity(
+            opacity: 0.4,
+            child: listTile,
+          ),
+          feedback: Text("${index + 1}"),
+        ),
+      );
+    }
+
+    listElements.add(
+      DragTarget(
+          onWillAccept: (value) => true,
+          onAccept: (value) =>
+              switchOrder(mapController, value as int, locations.length),
+          builder: (context, candidates, reject) {
+            if (candidates.isNotEmpty) {
+              int index = candidates[0] as int;
+              return ListTile(
+                trailing: const Icon(
+                  Icons.drag_handle,
+                  color: Colors.red,
+                ),
+                title: Text("${index + 1}"),
+                dense: true,
+              );
+            } else {
+              return const Divider();
+            }
+          }),
+    );
+
+    return ListView(children: listElements);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,11 +182,14 @@ class RoutePlanningPageState extends State<RoutePlanningPage> {
         compassViewPosition: CompassViewPosition.TopRight,
         onMapCreated: (MapboxMapController controller) =>
             mapController = controller,
-        onMapClick: (point, LatLng coordinates) =>
-            markLatLng(mapController, coordinates),
         onMapLongClick: (point, LatLng coordinates) =>
             extendLine(mapController, coordinates),
       )),
+      Container(
+        height: 350,
+        color: onPrimaryColorOf(context),
+        child: _buildDraggableList(locations),
+      ),
       Container(
           padding: const EdgeInsets.all(5),
           color: onPrimaryColorOf(context),
@@ -109,6 +219,7 @@ class RoutePlanningPageState extends State<RoutePlanningPage> {
               ElevatedButton(
                   style: ElevatedButton.styleFrom(primary: Colors.green[400]),
                   onPressed: null,
+                  //onPressed: () => updateLocations(mapController),
                   child: const Text("create")),
             ],
           ))
