3bef70d6f88e13f21a5c34a56c8024189faa87e7
Oliver Portee <oliver.portee@gmail.com>
2021-10-14 13:23:00 +0200

    split IntInput into two classes

--- sport-log-client/lib/pages/workout/strength_sessions/set_inputs/box_int_input.dart
+++ sport-log-client/lib/pages/workout/strength_sessions/set_inputs/box_int_input.dart
@@ -5,6 +5,79 @@ import 'package:flutter/services.dart';
 import 'package:sport_log/helpers/extensions/text_editing_controller_extension.dart';
 import 'package:sport_log/helpers/typedefs.dart';
 
+class _CaptionTextField extends StatelessWidget {
+  const _CaptionTextField({
+    Key? key,
+    required this.controller,
+    required this.focusNode,
+    required this.onChanged,
+    required this.caption,
+    required this.placeholder,
+    required this.formatFn,
+    required this.onSubmitted,
+    required this.onTap,
+    required this.width,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final ChangeCallback<String> onChanged;
+  final String caption;
+  final String placeholder;
+  final TextInputFormatFunction formatFn;
+  final VoidCallback onSubmitted;
+  final VoidCallback onTap;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      padding: const EdgeInsets.fromLTRB(8, 4, 8, 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            caption,
+            style: Theme.of(context)
+                .textTheme
+                .caption!
+                .copyWith(fontSize: IntInput.captionFontSize),
+          ),
+          _textField,
+        ],
+      ),
+    );
+  }
+
+  Widget get _textField {
+    return TextField(
+      controller: controller,
+      onChanged: onChanged,
+      onSubmitted: (_) => onSubmitted(),
+      focusNode: focusNode,
+      onTap: onTap,
+      textInputAction: TextInputAction.next,
+      inputFormatters: [
+        TextInputFormatter.withFunction(formatFn),
+      ],
+      keyboardType: TextInputType.number,
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: placeholder,
+        isDense: true,
+        contentPadding: EdgeInsets.zero,
+      ),
+      style: const TextStyle(
+        fontSize: IntInput.textFontSize,
+      ),
+      scrollPhysics: const NeverScrollableScrollPhysics(),
+    );
+  }
+}
+
 /// Text Field with box that only accepts non-negative ints
 class IntInput extends StatefulWidget {
   const IntInput({
@@ -54,55 +127,21 @@ class IntInputState extends State<IntInput> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: _widthPerDigit * widget.numberOfDigits,
-      padding: const EdgeInsets.fromLTRB(8, 4, 8, 0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(
-            widget.caption,
-            style: Theme.of(context)
-                .textTheme
-                .caption!
-                .copyWith(fontSize: IntInput.captionFontSize),
-          ),
-          _textField,
-        ],
-      ),
-    );
-  }
-
-  Widget get _textField {
-    return TextField(
+    return _CaptionTextField(
       controller: _controller,
+      focusNode: _focusNode,
       onChanged: _onChanged,
-      onSubmitted: (_) {
+      caption: widget.caption,
+      placeholder:
+          widget.placeholder.toString().padLeft(widget.numberOfDigits, '0'),
+      formatFn: _inputFormatter,
+      onSubmitted: () {
         if (widget.onSubmitted != null) {
           widget.onSubmitted!();
         }
       },
-      focusNode: _focusNode,
+      width: _widthPerDigit * widget.numberOfDigits,
       onTap: requestFocus,
-      textInputAction: TextInputAction.next,
-      inputFormatters: [
-        FilteringTextInputFormatter.digitsOnly,
-        TextInputFormatter.withFunction(_inputFormatter),
-      ],
-      keyboardType: TextInputType.number,
-      textAlign: TextAlign.right,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        hintText:
-            widget.placeholder.toString().padLeft(widget.numberOfDigits, '0'),
-        isDense: true,
-        contentPadding: EdgeInsets.zero,
-      ),
-      style: const TextStyle(
-        fontSize: IntInput.textFontSize,
-      ),
-      scrollPhysics: const NeverScrollableScrollPhysics(),
     );
   }
 
@@ -130,7 +169,11 @@ class IntInputState extends State<IntInput> {
     if (oldValue.text == newValue.text || newValue.text.isEmpty) {
       return newValue;
     }
-    final value = int.parse(newValue.text);
+    final maybeValue = int.tryParse(newValue.text);
+    if (maybeValue == null) {
+      return oldValue;
+    }
+    final int value = maybeValue;
 
     if (widget.maxValue != null && value > widget.maxValue! ||
         value >= pow(10, widget.numberOfDigits)) {
