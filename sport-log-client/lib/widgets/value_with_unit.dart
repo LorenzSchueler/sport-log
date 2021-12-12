b2c13422609982484ad2d7aba426f96bf777dfa4
Lorenz Schüler <lorenz@saarow.de>
2021-12-12 18:18:49 +0100

    add widget ValueWithUnit

---
+++ sport-log-client/lib/widgets/value_with_unit.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ValueWithUnit extends StatelessWidget {
+  final String value;
+  final String unit;
+
+  const ValueWithUnit({
+    Key? key,
+    required this.value,
+    required this.unit,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+          text: "$value ",
+          style: const TextStyle(fontSize: 20),
+        ),
+        TextSpan(
+          text: unit,
+          style: const TextStyle(fontSize: 14),
+        )
+      ]),
+      textAlign: TextAlign.center,
+    );
+  }
+}
