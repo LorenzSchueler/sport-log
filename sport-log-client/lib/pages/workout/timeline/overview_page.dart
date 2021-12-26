41af09fe2f26519fd4f258804a00a30daab44ac6
Lorenz Schüler <lorenz@saarow.de>
2021-12-26 18:28:04 +0100

    show basic timeline

--- sport-log-client/lib/pages/workout/timeline/overview_page.dart
+++ sport-log-client/lib/pages/workout/timeline/overview_page.dart
@@ -1,6 +1,15 @@
+import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
+import 'package:sport_log/data_provider/user_state.dart';
+import 'package:sport_log/helpers/formatting.dart';
+import 'package:sport_log/helpers/id_generation.dart';
 import 'package:sport_log/helpers/logger.dart';
+import 'package:sport_log/models/all.dart';
+import 'package:sport_log/models/cardio/cardio_session.dart';
+import 'package:sport_log/models/cardio/position.dart';
+import 'package:sport_log/models/diary/diary.dart';
 import 'package:sport_log/models/movement/movement.dart';
+import 'package:sport_log/models/timeline_union.dart';
 import 'package:sport_log/pages/workout/date_filter/date_filter_state.dart';
 import 'package:sport_log/pages/workout/date_filter/date_filter_widget.dart';
 import 'package:sport_log/pages/workout/session_tab_utils.dart';
@@ -17,6 +26,75 @@ class TimelinePage extends StatefulWidget {
 
 class TimelinePageState extends State<TimelinePage> {
   final _logger = Logger('TimelinePage');
+
+  final List<TimelineUnion> _items = [
+    TimelineUnion.strengthSession(StrengthSession(
+        id: randomId(),
+        userId: UserState.instance.currentUser!.id,
+        datetime: DateTime.now(),
+        movementId: Int64(1),
+        interval: const Duration(seconds: 120),
+        comments: null,
+        deleted: false)),
+    TimelineUnion.metconSession(MetconSession(
+        id: randomId(),
+        userId: UserState.instance.currentUser!.id,
+        metconId: Int64(1),
+        datetime: DateTime.now(),
+        time: 300,
+        rounds: 3,
+        reps: 0,
+        rx: true,
+        comments: null,
+        deleted: false)),
+    TimelineUnion.cardioSession(
+      CardioSession(
+          id: randomId(),
+          userId: UserState.instance.currentUser!.id,
+          movementId: Int64(1),
+          cardioType: CardioType.training,
+          datetime: DateTime.now(),
+          distance: 15034,
+          ascent: 308,
+          descent: 297,
+          time: 4189,
+          calories: null,
+          track: [
+            Position(
+                longitude: 11.33,
+                latitude: 47.27,
+                elevation: 600,
+                distance: 0,
+                time: 0),
+            Position(
+                longitude: 11.331,
+                latitude: 47.27,
+                elevation: 650,
+                distance: 1000,
+                time: 200),
+            Position(
+                longitude: 11.33,
+                latitude: 47.272,
+                elevation: 600,
+                distance: 2000,
+                time: 500)
+          ],
+          avgCadence: 167,
+          cadence: null,
+          avgHeartRate: 189,
+          heartRate: null,
+          routeId: null,
+          comments: null,
+          deleted: false),
+    ),
+    TimelineUnion.diary(Diary(
+        id: randomId(),
+        userId: UserState.instance.currentUser!.id,
+        date: DateTime.now().subtract(const Duration(days: 3)),
+        bodyweight: 80.0,
+        comments: "bla",
+        deleted: false)),
+  ];
 
   DateFilterState _dateFilter = MonthFilter.current();
   Movement? _movement;
@@ -61,11 +139,45 @@ class TimelinePageState extends State<TimelinePage> {
           ),
         ),
       ),
-      body: const Center(child: Text('Not implemented yet :(')),
+      body: ListView.separated(
+          itemBuilder: _buildItemEntry,
+          itemCount: _items.length,
+          separatorBuilder: (_, __) => const Divider()),
       bottomNavigationBar:
           SessionTabUtils.bottomNavigationBar(context, sessionsPageTab),
       drawer: MainDrawer(selectedRoute: route),
       floatingActionButton: null,
     );
+  }
+
+  Widget _buildItemEntry(BuildContext context, int index) {
+    final TimelineUnion item = _items[index];
+
+    return GestureDetector(
+        onTap: () {
+          showDetails(context, item);
+        },
+        child: _itemCard(item));
+  }
+
+  Widget _itemCard(TimelineUnion item) {
+    return item.map(
+        (strengthSession) => Text(formatDate(strengthSession.datetime)),
+        (metconSession) => Text(formatDate(metconSession.datetime)),
+        (cardioSession) => Text(formatDate(cardioSession.datetime)),
+        (diary) => Text(formatDate(diary.date)));
+  }
+
+  void showDetails(BuildContext context, TimelineUnion item) {
+    item.map(
+        (strengthSession) => Navigator.of(context)
+            .pushNamed(Routes.diary.edit, arguments: strengthSession),
+        (metconSession) => Navigator.of(context)
+            .pushNamed(Routes.metcon.edit, arguments: metconSession),
+        (cardioSession) => Navigator.of(context)
+            .pushNamed(Routes.cardio.cardioDetails, arguments: cardioSession),
+        (diary) => Navigator.of(context)
+            .pushNamed(Routes.diary.edit, arguments: diary));
+    ;
   }
 }
