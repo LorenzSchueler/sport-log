4116e74ee87ce8fee340099dd951f5d86afc34b6
Lorenz Schüler <lorenz@saarow.de>
2022-01-23 20:54:08 +0100

    full screen map

--- sport-log-client/lib/pages/map/map_page.dart
+++ sport-log-client/lib/pages/map/map_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:sport_log/defaults.dart';
 import 'package:sport_log/helpers/logger.dart';
 import 'package:sport_log/routes.dart';
+import 'package:sport_log/secrets.dart';
 import 'package:sport_log/widgets/main_drawer.dart';
 
 class MapPage extends StatefulWidget {
@@ -13,11 +17,42 @@ class MapPage extends StatefulWidget {
 class MapPageState extends State<MapPage> {
   final _logger = Logger('MapPage');
 
+  late MapboxMapController _sessionMapController;
+  bool overlaysVisible = false;
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) async {
+      _logger.i("overlays $systemOverlaysAreVisible");
+      overlaysVisible = systemOverlaysAreVisible;
+    }); // TODO
+
     return Scaffold(
-        appBar: AppBar(title: const Text("Map")),
-        drawer: const MainDrawer(selectedRoute: Routes.map),
-        body: Text("not implemented"));
+      extendBodyBehindAppBar: true,
+      appBar:
+          //overlaysVisible ?
+          AppBar(
+        backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+        elevation: 0,
+      ),
+      //: null,
+      drawer: const MainDrawer(selectedRoute: Routes.map),
+      body: MapboxMap(
+        accessToken: Secrets.mapboxAccessToken,
+        styleString: Defaults.mapbox.style.outdoor,
+        initialCameraPosition: const CameraPosition(
+          zoom: 13.0,
+          target: LatLng(47.27, 11.33),
+        ),
+        onMapCreated: (MapboxMapController controller) =>
+            _sessionMapController = controller,
+      ),
+    );
+  }
+
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
   }
 }
