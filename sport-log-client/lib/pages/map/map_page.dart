ebc4cb35c65ae97e23c9384e83d9e0eb62352698
Lorenz Schüler <lorenz@saarow.de>
2022-02-08 20:54:20 +0100

    use expandable fab in map page and hide overlays onTab

--- sport-log-client/lib/pages/map/map_page.dart
+++ sport-log-client/lib/pages/map/map_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:sport_log/defaults.dart';
 import 'package:sport_log/helpers/logger.dart';
-import 'package:sport_log/helpers/theme.dart';
 import 'package:sport_log/routes.dart';
+import 'package:sport_log/widgets/expandable_fab.dart';
 import 'package:sport_log/widgets/main_drawer.dart';
 
 class MapPage extends StatefulWidget {
@@ -18,28 +18,22 @@ class MapPageState extends State<MapPage> {
   final _logger = Logger('MapPage');
 
   late MapboxMapController _sessionMapController;
-  bool overlaysVisible = false;
+  bool showOverlays = true;
 
   String mapStyle = Defaults.mapbox.style.outdoor;
-  bool showMapStyleButtons = true;
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) async {
-      _logger.i("overlays $systemOverlaysAreVisible");
-      overlaysVisible = systemOverlaysAreVisible;
-    }); // TODO
 
     return Scaffold(
         extendBodyBehindAppBar: true,
-        appBar:
-            //overlaysVisible ?
-            AppBar(
-          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
-          elevation: 0,
-        ),
-        //: null,
+        appBar: showOverlays
+            ? AppBar(
+                backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+                elevation: 0,
+              )
+            : null,
         drawer: const MainDrawer(selectedRoute: Routes.map),
         body: Stack(children: [
           MapboxMap(
@@ -51,75 +45,35 @@ class MapPageState extends State<MapPage> {
             ),
             onMapCreated: (MapboxMapController controller) =>
                 _sessionMapController = controller,
+            onMapClick: (_, __) => setState(() {
+              showOverlays = !showOverlays;
+            }),
           ),
-          showMapStyleButtons // TODO
-              ? Positioned(
-                  top: 10,
-                  right: 5,
-                  child: Container(
-                    //height: 50,
-                    //width: 200,
-
-                    width: 50,
-                    height: 200,
-                    decoration: BoxDecoration(
-                        color: const Color.fromARGB(150, 0, 0, 0),
-                        borderRadius: Defaults.borderRadius.normal),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                //padding: EdgeInsets.all(0),
-                                shape: const CircleBorder(),
-                              ),
-                              onPressed: () {
-                                _logger.i("hide map style buttons");
-                                setState(() {
-                                  showMapStyleButtons = false;
-                                  mapStyle = Defaults.mapbox.style.outdoor;
-                                });
-                              },
-                              child: const Icon(Icons.map)),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                              ),
-                              onPressed: () {
-                                _logger.i("hide map style buttons");
-                                setState(() {
-                                  showMapStyleButtons = false;
-                                  mapStyle = Defaults.mapbox.style.satellite;
-                                });
-                              },
-                              child: const Icon(Icons.map)),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                              ),
-                              onPressed: () {
-                                _logger.i("hide map style buttons");
-                                setState(() {
-                                  showMapStyleButtons = false;
-                                  mapStyle = Defaults.mapbox.style.street;
-                                });
-                              },
-                              child: const Icon(Icons.map)),
-                        ]),
-                  ))
-              : Positioned(
-                  top: 10,
-                  right: 5,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                      ),
-                      onPressed: () => setState(() {
-                            _logger.i("show map style buttons");
-                            showMapStyleButtons = true;
-                          }),
-                      child: const Icon(Icons.map)),
-                ),
+          if (showOverlays)
+            Positioned(
+                top: 5,
+                right: 5,
+                child: ExpandableFab(
+                    horizontal: true,
+                    icon: const Icon(Icons.map),
+                    items: [
+                      ExpandableFabItem(
+                          icon: const Icon(Icons.map),
+                          onPressed: () => setState(() {
+                                mapStyle = Defaults.mapbox.style.outdoor;
+                              })),
+                      ExpandableFabItem(
+                          icon: const Icon(Icons.map),
+                          onPressed: () => setState(() {
+                                mapStyle = Defaults.mapbox.style.street;
+                              })),
+                      ExpandableFabItem(
+                          // TODO does not react
+                          icon: const Icon(Icons.map),
+                          onPressed: () => setState(() {
+                                mapStyle = Defaults.mapbox.style.satellite;
+                              })),
+                    ])),
         ]));
   }
 
