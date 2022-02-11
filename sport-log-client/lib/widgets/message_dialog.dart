7e7176e351fc1e3771ac4a8607974003897f85c8
Lorenz Schüler <lorenz@saarow.de>
2022-02-11 20:55:04 +0100

    create widget MessageDialog

---
+++ sport-log-client/lib/widgets/message_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+Future<void> showMessageDialog({
+  required BuildContext context,
+  required String? title,
+  required String text,
+}) async {
+  return showDialog<void>(
+    context: context,
+    builder: (_) => MessageDialog(
+      title: title,
+      text: text,
+    ),
+  );
+}
+
+class MessageDialog extends StatelessWidget {
+  final String? title;
+  final String text;
+
+  const MessageDialog({
+    Key? key,
+    required this.title,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: title == null ? null : Text(title!),
+      content: Text(text),
+      actions: [
+        TextButton(
+            onPressed: () => Navigator.pop(context), child: const Text("Ok")),
+      ],
+    );
+  }
+}
