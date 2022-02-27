5a4af29e9675d3e420cbd5f94ac0642e9660aaeb
Lorenz Schüler <lorenz@saarow.de>
2022-02-27 14:05:48 +0100

    refactor movementOverviewPage

--- sport-log-client/lib/pages/movements/movement_overview_page.dart
+++ sport-log-client/lib/pages/movements/movement_overview_page.dart
@@ -1,16 +1,16 @@
 import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
-import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
-import 'package:implicitly_animated_reorderable_list/transitions.dart';
 import 'package:sport_log/api/api.dart';
 import 'package:sport_log/data_provider/data_providers/movement_data_provider.dart';
-import 'package:sport_log/helpers/extensions/list_extension.dart';
-import 'package:sport_log/helpers/page_return.dart';
+import 'package:sport_log/helpers/logger.dart';
+import 'package:sport_log/helpers/snackbar.dart';
 import 'package:sport_log/models/movement/all.dart';
 import 'package:sport_log/routes.dart';
 import 'package:sport_log/widgets/app_icons.dart';
 import 'package:sport_log/widgets/approve_dialog.dart';
 import 'package:sport_log/widgets/main_drawer.dart';
+
+final _dataProvider = MovementDescriptionDataProvider.instance;
 
 class MovementsPage extends StatefulWidget {
   const MovementsPage({Key? key}) : super(key: key);
@@ -20,49 +20,29 @@ class MovementsPage extends StatefulWidget {
 }
 
 class _MovementsPageState extends State<MovementsPage> {
-  final _dataProvider = MovementDataProvider.instance;
+  final _logger = Logger('MovementsPage');
   List<MovementDescription> _movementDescriptions = [];
 
   @override
   void initState() {
     super.initState();
-    _dataProvider.getMovementDescriptions().then((mds) {
-      setState(() {
-        _movementDescriptions = mds;
-      });
-    });
+    _dataProvider.addListener(_update);
+    _dataProvider.onNoInternetConnection =
+        () => showSimpleSnackBar(context, 'No Internet connection.');
+    _update();
   }
 
-  void _handlePageReturn(dynamic object) {
-    if (object is ReturnObject<MovementDescription>) {
-      switch (object.action) {
-        case ReturnAction.created:
-          setState(() {
-            _movementDescriptions.add(object.payload);
-            _movementDescriptions.sortBy((m) => m.movement.name.toUpperCase());
-            // TODO: "hide" server defined movements with same name/dimension
-          });
-          break;
-        case ReturnAction.updated:
-          setState(() {
-            _movementDescriptions.update(
-              object.payload,
-              by: (o) => o.movement.id,
-            );
-            _movementDescriptions.sortBy((m) => m.movement.name.toUpperCase());
-            // TODO: "hide" server defined movements with same name/dimension
-          });
-          break;
-        case ReturnAction.deleted:
-          setState(
-            () => _movementDescriptions.delete(
-              object.payload,
-              by: (m) => m.movement.id,
-            ),
-          );
-        // TODO: "unhide" server defined movements with same name/dimension
-      }
-    }
+  @override
+  void dispose() {
+    _dataProvider.removeListener(_update);
+    _dataProvider.onNoInternetConnection = null;
+    super.dispose();
+  }
+
+  Future<void> _update() async {
+    _logger.d('Updating movement page');
+    final movementDescriptions = await _dataProvider.getNonDeleted();
+    setState(() => _movementDescriptions = movementDescriptions);
   }
 
   Future<void> _pullFromServer() async {
@@ -71,10 +51,6 @@ class _MovementsPageState extends State<MovementsPage> {
         ScaffoldMessenger.of(context)
             .showSnackBar(SnackBar(content: Text(error.toErrorMessage())));
       }
-    });
-    final mds = await _dataProvider.getMovementDescriptions();
-    setState(() {
-      _movementDescriptions = mds;
     });
   }
 
@@ -87,94 +63,82 @@ class _MovementsPageState extends State<MovementsPage> {
       drawer: MainDrawer(selectedRoute: Routes.movement.overview),
       body: RefreshIndicator(
         onRefresh: _pullFromServer,
-        child: _body(context),
+        child: _movementDescriptions.isEmpty
+            ? const Center(
+                child: Text(
+                  "looks like there are no movements there yet 😔 \npress ＋ to create a new one",
+                  textAlign: TextAlign.center,
+                ),
+              )
+            : ListView.builder(
+                itemBuilder: (_, index) => MovementCard(
+                    movementDescription: _movementDescriptions[index]),
+                itemCount: _movementDescriptions.length,
+              ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(AppIcons.add),
         onPressed: () async {
-          final returnObj =
-              await Navigator.pushNamed(context, Routes.movement.edit);
-          _handlePageReturn(returnObj);
+          await Navigator.pushNamed(context, Routes.movement.edit);
         },
       ),
     );
   }
+}
 
-  Widget _body(BuildContext context) {
-    if (_movementDescriptions.isEmpty) {
-      return const Center(child: Text("No movements there."));
-    }
-    return ImplicitlyAnimatedList(
-      items: _movementDescriptions,
-      itemBuilder: _movementToWidget,
-      areItemsTheSame: MovementDescription.areTheSame,
-    );
-  }
+class MovementCard extends StatelessWidget {
+  final MovementDescription movementDescription;
 
-  Widget _movementToWidget(
-    BuildContext context,
-    Animation<double> animation,
-    MovementDescription md,
-    int index,
-  ) {
-    final movement = md.movement;
-    return SizeFadeTransition(
-      key: ValueKey(movement.id),
-      animation: animation,
-      child: ExpansionTileCard(
-        leading: CircleAvatar(child: Text(movement.name[0])),
-        title: Text(movement.name),
-        subtitle: Text(movement.dimension.displayName),
-        children: [
-          if (movement.description != null) const Divider(),
-          if (movement.description != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-              child: Text(movement.description!),
-            ),
-          if (movement.userId != null) const Divider(),
-          if (movement.userId != null)
-            ButtonBar(
-              alignment: MainAxisAlignment.spaceAround,
-              children: [
-                if (!md.hasReference)
-                  IconButton(
-                    onPressed: () {
-                      assert(movement.userId != null && !md.hasReference);
-                      _dataProvider.deleteSingle(movement);
-                      setState(
-                        () => _movementDescriptions.delete(
-                          md,
-                          by: (m) => m.movement.id,
-                        ),
-                      );
-                    },
-                    icon: const Icon(AppIcons.delete),
-                  ),
-                IconButton(
-                  onPressed: () async {
-                    assert(movement.userId != null);
-                    if (md.hasReference) {
-                      final bool? approved = await showApproveDialog(
-                        context,
-                        'Warning',
-                        'Changes will be reflected in existing workouts.',
-                      );
-                      if (approved == null || !approved) return;
-                    }
-                    final returnObj = await Navigator.pushNamed(
-                      context,
-                      Routes.movement.edit,
-                      arguments: md.copy(),
-                    );
-                    _handlePageReturn(returnObj);
-                  },
-                  icon: const Icon(AppIcons.edit),
-                ),
-              ],
-            ),
+  const MovementCard({Key? key, required this.movementDescription})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTileCard(
+      leading: CircleAvatar(child: Text(movementDescription.movement.name[0])),
+      title: Text(movementDescription.movement.name),
+      subtitle: Text(movementDescription.movement.dimension.displayName),
+      children: [
+        if (movementDescription.movement.description != null) ...[
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Text(movementDescription.movement.description!),
+          ),
         ],
-      ),
+        if (movementDescription.movement.userId != null) ...[
+          const Divider(),
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceAround,
+            children: [
+              if (!movementDescription.hasReference)
+                IconButton(
+                  onPressed: () async =>
+                      await _dataProvider.deleteSingle(movementDescription),
+                  icon: const Icon(AppIcons.delete),
+                ),
+              IconButton(
+                onPressed: () async {
+                  if (movementDescription.hasReference) {
+                    final bool? approved = await showApproveDialog(
+                      context,
+                      'Warning',
+                      'Changes will be reflected in existing workouts.',
+                    );
+                    if (approved == null || !approved) return;
+                  }
+                  await Navigator.pushNamed(
+                    context,
+                    Routes.movement.edit,
+                    arguments: movementDescription,
+                  );
+                },
+                icon: const Icon(AppIcons.edit),
+              ),
+            ],
+          ),
+        ],
+      ],
     );
   }
 }
