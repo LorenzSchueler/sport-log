f85bda6b0347bad7542fb7b2bfb980e268f63014
Lorenz Schüler <lorenz@saarow.de>
2022-03-02 14:16:19 +0100

    move CountWeightPicker to new file

--- sport-log-client/lib/pages/workout/strength_sessions/new_set_input.dart
+++ sport-log-client/lib/pages/workout/strength_sessions/new_set_input.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sport_log/defaults.dart';
-import 'package:sport_log/helpers/theme.dart';
 import 'package:sport_log/models/all.dart';
 import 'package:sport_log/pages/workout/strength_sessions/set_inputs/set_duration_input.dart';
-import 'package:sport_log/widgets/form_widgets/double_picker.dart';
-import 'package:sport_log/widgets/form_widgets/int_picker.dart';
-import 'package:sport_log/widgets/app_icons.dart';
+import 'package:sport_log/widgets/form_widgets/count_weight_picker.dart';
 
 class NewSetInput extends StatelessWidget {
   const NewSetInput({
@@ -37,107 +33,5 @@ class NewSetInput extends StatelessWidget {
           setValue: onNewSet,
         );
     }
-  }
-}
-
-class CountWeightPicker extends StatefulWidget {
-  const CountWeightPicker({
-    required this.setValue,
-    required this.countLabel,
-    this.countUnit,
-    Key? key,
-  }) : super(key: key);
-
-  final String countLabel;
-  final String? countUnit;
-  final void Function(int count, [double? weight]) setValue;
-
-  @override
-  _CountWeightPickerState createState() => _CountWeightPickerState();
-}
-
-class _CountWeightPickerState extends State<CountWeightPicker> {
-  int _count = 0;
-  double? _weight;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Table(
-          columnWidths: const {
-            0: FixedColumnWidth(30),
-            1: FixedColumnWidth(140),
-            2: FixedColumnWidth(166),
-          },
-          children: [
-            TableRow(
-              children: [
-                Container(),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: Text(
-                    widget.countUnit == null
-                        ? widget.countLabel
-                        : "${widget.countLabel} (${widget.countUnit!})",
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ),
-                IntPicker(
-                  setValue: (count) => setState(() => _count = count),
-                ),
-              ],
-            ),
-            TableRow(
-              children: _weight == null
-                  ? [
-                      IconButton(
-                        icon: const Icon(AppIcons.add),
-                        onPressed: () => setState(() => _weight = 0),
-                        padding: EdgeInsets.zero,
-                      ),
-                      TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.middle,
-                        child: Text(
-                          "Weight ",
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                      ),
-                      Container(),
-                    ]
-                  : [
-                      IconButton(
-                        icon: const Icon(AppIcons.close),
-                        onPressed: () => setState(() => _weight = null),
-                        padding: EdgeInsets.zero,
-                      ),
-                      TableCell(
-                        verticalAlignment: TableCellVerticalAlignment.middle,
-                        child: Text(
-                          "Weight ",
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                      ),
-                      DoublePicker(
-                        setValue: (weight) => setState(() => _weight = weight),
-                      )
-                    ],
-            )
-          ],
-        ),
-        Defaults.sizedBox.horizontal.normal,
-        IconButton(
-          icon: const Icon(AppIcons.check),
-          iconSize: 40,
-          color: _count > 0 ? primaryColorOf(context) : null,
-          onPressed: _count > 0 ? () => widget.setValue(_count, _weight) : null,
-        ),
-      ],
-    );
   }
 }
--- sport-log-client/lib/pages/workout/strength_sessions/set_inputs/set_duration_input.dart
+++ sport-log-client/lib/pages/workout/strength_sessions/set_inputs/set_duration_input.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sport_log/defaults.dart';
+import 'package:sport_log/helpers/extensions/text_editing_controller_extension.dart';
 import 'package:sport_log/helpers/theme.dart';
-import 'package:sport_log/pages/workout/strength_sessions/set_inputs/box_int_input.dart';
+import 'package:sport_log/helpers/typedefs.dart';
 import 'package:sport_log/widgets/app_icons.dart';
 
 class SetDurationInput extends StatefulWidget {
@@ -60,7 +65,7 @@ class _SetDurationInputState extends State<SetDurationInput> {
     return Row(
       children: [
         _timeInput,
-        const Spacer(),
+        Defaults.sizedBox.horizontal.normal,
         _addButton,
       ],
     );
@@ -140,6 +145,198 @@ class _SetDurationInputState extends State<SetDurationInput> {
       caption: 'ms',
       numberOfDigits: 3,
       onChanged: (value) => setState(() => _milliseconds = value),
+    );
+  }
+}
+
+class _CaptionTextField extends StatelessWidget {
+  const _CaptionTextField({
+    Key? key,
+    required this.controller,
+    required this.focusNode,
+    required this.onChanged,
+    required this.caption,
+    required this.placeholder,
+    required this.formatFn,
+    this.onSubmitted,
+    required this.onTap,
+    required this.width,
+    required this.scrollable,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final ChangeCallback<String> onChanged;
+  final String caption;
+  final String placeholder;
+  final TextInputFormatFunction formatFn;
+  final VoidCallback? onSubmitted;
+  final VoidCallback onTap;
+  final double width;
+  final bool scrollable;
+
+  static const double captionFontSize = 15;
+  static const double textFontSize = 40;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      padding: const EdgeInsets.fromLTRB(8, 4, 8, 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            caption,
+            style: Theme.of(context)
+                .textTheme
+                .caption!
+                .copyWith(fontSize: captionFontSize),
+          ),
+          _textField,
+        ],
+      ),
+    );
+  }
+
+  Widget get _textField {
+    return TextField(
+      controller: controller,
+      onChanged: onChanged,
+      onSubmitted: (_) {
+        if (onSubmitted != null) {
+          onSubmitted!();
+        }
+      },
+      focusNode: focusNode,
+      onTap: onTap,
+      textInputAction: TextInputAction.next,
+      inputFormatters: [
+        TextInputFormatter.withFunction(formatFn),
+      ],
+      keyboardType: TextInputType.number,
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: placeholder,
+        isDense: true,
+        contentPadding: EdgeInsets.zero,
+      ),
+      style: const TextStyle(
+        fontSize: textFontSize,
+      ),
+      scrollPhysics: scrollable ? null : const NeverScrollableScrollPhysics(),
+    );
+  }
+}
+
+/// Text Field with box that only accepts non-negative ints
+class PaddedIntInput extends StatefulWidget {
+  const PaddedIntInput({
+    Key? key,
+    required this.placeholder,
+    required this.onChanged,
+    required this.caption,
+    required this.numberOfDigits,
+    this.submitOnDigitsReached = false,
+    // TODO: not necessary
+    this.onSubmitted,
+    this.maxValue,
+  })  : assert(numberOfDigits > 0),
+        super(key: key);
+
+  final int placeholder;
+  final ChangeCallback<int> onChanged;
+  final VoidCallback? onSubmitted;
+  final String caption;
+  final int numberOfDigits;
+  final int? maxValue;
+  final bool submitOnDigitsReached;
+
+  static double get fontSize => _CaptionTextField.textFontSize;
+
+  @override
+  PaddedIntInputState createState() => PaddedIntInputState();
+}
+
+class PaddedIntInputState extends State<PaddedIntInput> {
+  static const double _widthPerDigit = 31;
+
+  final _focusNode = FocusNode();
+  final _controller = TextEditingController();
+
+  void requestFocus() {
+    _focusNode.requestFocus();
+    _controller.selectAll();
+  }
+
+  void clear() {
+    _controller.clear();
+    widget.onChanged(widget.placeholder);
+  }
+
+  bool get hasFocus => _focusNode.hasFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return _CaptionTextField(
+      controller: _controller,
+      focusNode: _focusNode,
+      onChanged: _onChanged,
+      caption: widget.caption,
+      placeholder:
+          widget.placeholder.toString().padLeft(widget.numberOfDigits, '0'),
+      formatFn: _inputFormatter,
+      onSubmitted: widget.onSubmitted,
+      width: _widthPerDigit * widget.numberOfDigits,
+      onTap: requestFocus,
+      scrollable: false,
+    );
+  }
+
+  void _onChanged(String text) {
+    if (_controller.text.trim().isEmpty) {
+      widget.onChanged(widget.placeholder);
+      return;
+    }
+    final maybeValue = int.tryParse(_controller.text.trim());
+    assert(maybeValue != null);
+    if (maybeValue == null) {
+      return;
+    }
+    assert(widget.maxValue == null || maybeValue <= widget.maxValue!);
+    widget.onChanged(maybeValue);
+    if (widget.submitOnDigitsReached &&
+        maybeValue >= pow(10, widget.numberOfDigits - 1) &&
+        widget.onSubmitted != null) {
+      widget.onSubmitted!();
+    }
+  }
+
+  TextEditingValue _inputFormatter(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    if (oldValue.text == newValue.text || newValue.text.isEmpty) {
+      return newValue;
+    }
+    final maybeValue = int.tryParse(newValue.text);
+    if (maybeValue == null) {
+      return oldValue;
+    }
+    final int value = maybeValue;
+
+    if (widget.maxValue != null && value > widget.maxValue! ||
+        value >= pow(10, widget.numberOfDigits)) {
+      return oldValue;
+    }
+    assert(newValue.selection.isCollapsed);
+    final cursorPos = newValue.text.length - newValue.selection.start;
+    final newText = value.toString().padLeft(widget.numberOfDigits, '0');
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(offset: newText.length - cursorPos),
     );
   }
 }
