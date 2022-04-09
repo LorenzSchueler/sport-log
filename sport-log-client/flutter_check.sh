0640648f35655b2cfd6c723a5a5255abfca9d738
Lorenz Schüler <lorenz@saarow.de>
2022-04-09 10:50:47 +0200

    split gh actions for flutter

--- sport-log-client/flutter_check.sh
+++ sport-log-client/flutter_check.sh
@@ -1,5 +1,6 @@
 flutter pub get
 flutter format --set-exit-if-changed .
 flutter analyze
-# flutter test # needs SDK_REGISTRY_TOKEN in env args
 # flutter build apk # needs SDK_REGISTRY_TOKEN in env args
+# flutter test # needs SDK_REGISTRY_TOKEN in env args
+jscpd --min-lines 20 --reporters consoleFull lib/
