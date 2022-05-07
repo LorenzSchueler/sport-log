36dcca824f0242fa88af8d726ded2dbac24d7963
Lorenz Schüler <lorenz@saarow.de>
2022-05-07 13:30:20 +0200

    add sort extension

---
+++ sport-log-client/lib/helpers/extensions/sort_extension.dart
@@ -0,0 +1,23 @@
+import 'package:fuzzywuzzy/applicable.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
+
+class _SortAlgorithm implements Applicable {
+  @override
+  int apply(String s1, String s2) => tokenSortRatio(s1, s2);
+}
+
+extension SortExtension<T> on List<T> {
+  List<T> sortByKey({
+    required String? key,
+    required String Function(T) toString,
+  }) {
+    return key == null || key.isEmpty
+        ? this
+        : extractAllSorted(
+            query: key,
+            choices: this,
+            getter: toString,
+            ratio: _SortAlgorithm(),
+          ).map((e) => e.choice).toList();
+  }
+}
