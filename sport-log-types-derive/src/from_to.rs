LorenzSchueler/sport-log

commit 55bb030928ca65f8267f2e1174cc001ab7a9d130

refactor: remove ToI64 & FromI64

sport-log-types-derive/src/lib.rs
@@ -199,38 +199,20 @@ pub fn verify_unchecked_derive(input: TokenStream) -> TokenStream {
     impl_verify_unchecked(&ast)
 }
 
-/// Derives `sport_log_types::FromI64`.
-///
-/// This macro only works if the type is a one tuple struct of i64.
-#[proc_macro_derive(FromI64)]
-pub fn form_i64_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    impl_from_i64(&ast)
-}
-
-/// Derives `sport_log_types::ToI64`.
-///
-/// This macro only works if the type is a one tuple struct of i64.
-#[proc_macro_derive(ToI64)]
-pub fn to_i64_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    impl_to_i64(&ast)
-}
-
 /// Derives `diesel::types::ToSql<diesel::sql_types::BigInt, diesel::pg::Pg>`.
 ///
-/// This macro only works if the type implements `sport_log_types::FromI64` which can also be derived using [FromI64].
-#[proc_macro_derive(ToSql)]
+/// This macro only works if the type is a one tuple struct of i64.
+#[proc_macro_derive(IdToSql)]
 pub fn unverified_inner_int_to_sql(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
-    impl_to_sql(&ast)
+    impl_id_to_sql(&ast)
 }
 
-/// Derives `diesel::types::FromSql<diesel::sql_types::BigInt, diesel::pg::Pg>`.
+/// Derives `diesel::types::IdFromSql<diesel::sql_types::BigInt, diesel::pg::Pg>`.
 ///
-/// This macro only works if the type implements `sport_log_types::FromI64` which can also be derived using [FromI64].
-#[proc_macro_derive(FromSql)]
+/// This macro only works if the type is a one tuple struct of i64.
+#[proc_macro_derive(IdFromSql)]
 pub fn unverified_inner_int_from_sql(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
-    impl_from_sql(&ast)
+    impl_id_from_sql(&ast)
 }

commit f071033ba321554e338ab5522b7e10a298df0161

fix: serialize ids to IdString in server (broken in 55bb030)

sport-log-types-derive/src/from_to.rs
@@ -27,3 +27,27 @@ pub fn impl_id_from_sql(ast: &syn::DeriveInput) -> TokenStream {
     };
     gen.into()
 }
+
+pub fn impl_id_string(ast: &syn::DeriveInput) -> TokenStream {
+    let typename = &ast.ident;
+
+    let gen = quote! {
+        impl TryFrom<crate::types::IdString> for #typename {
+            type Error = <i64 as std::str::FromStr>::Err;
+
+            fn try_from(id_string: IdString) -> Result<Self, Self::Error> {
+                use std::str::FromStr;
+
+                id_string.0.parse().map(Self)
+            }
+        }
+
+        #[allow(clippy::from_over_into)]
+        impl Into<crate::types::IdString> for #typename {
+            fn into(self) -> crate::types::IdString {
+                crate::types::IdString(self.0.to_string())
+            }
+        }
+    };
+    gen.into()
+}

sport-log-types-derive/src/lib.rs
@@ -3,10 +3,10 @@
 use proc_macro::TokenStream;
 
 mod crud;
-mod to_from_sql;
+mod from_to;
 mod verification;
 use crud::*;
-use to_from_sql::*;
+use from_to::*;
 use verification::*;
 
 /// Derives `sport_log_types::Create`.
@@ -215,4 +215,13 @@ pub fn unverified_inner_int_to_sql(input: TokenStream) -> TokenStream {
 pub fn unverified_inner_int_from_sql(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     impl_id_from_sql(&ast)
+}
+
+/// Derives `TryFrom<IdString>` and `Into<IdString>`.
+///
+/// This macro only works if the type is a one tuple struct of i64.
+#[proc_macro_derive(IdString)]
+pub fn unverified_id_string(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_id_string(&ast)
 }

sport-log-types/src/types/action.rs
@@ -4,6 +4,7 @@ use diesel::sql_types::BigInt;
 #[cfg(feature = "server")]
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckAPId, CheckUserId, Create, GetAll, GetById, GetByIds, GetBySync, GetByUser, GetByUserSync,
@@ -18,10 +19,10 @@ use crate::{
     schema::{action, action_event, action_provider, action_rule},
     Platform, User,
 };
-use crate::{PlatformId, UserId};
+use crate::{types::IdString, PlatformId, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(
@@ -68,8 +69,8 @@ pub struct ActionProvider {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(
@@ -118,7 +119,7 @@ pub struct Action {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "server", derive(DbEnum))]
 
 pub enum Weekday {
@@ -145,8 +146,8 @@ impl Weekday {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUser),
@@ -189,8 +190,8 @@ pub struct ActionRule {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(

sport-log-types/src/types/cardio.rs
@@ -10,6 +10,7 @@ use diesel::{
 #[cfg(feature = "server")]
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql,
@@ -21,9 +22,9 @@ use crate::{
     schema::{cardio_blueprint, cardio_session, route},
     TrainingPlan, User,
 };
-use crate::{Movement, MovementId, TrainingPlanId, UserId};
+use crate::{types::IdString, Movement, MovementId, TrainingPlanId, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "server", derive(DbEnum))]
 pub enum CardioType {
     Training,
@@ -90,8 +91,8 @@ impl FromSql<crate::schema::sql_types::Position, Pg> for Position {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -138,8 +139,8 @@ pub struct Route {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -195,8 +196,8 @@ pub struct CardioBlueprint {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),

sport-log-types/src/types/diary_wod.rs
@@ -2,6 +2,7 @@ use chrono::NaiveDate;
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql,
@@ -9,15 +10,15 @@ use sport_log_types_derive::{
     VerifyUnchecked,
 };
 
-use crate::UserId;
 #[cfg(feature = "server")]
 use crate::{
     schema::{diary, wod},
     User,
 };
+use crate::{types::IdString, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -60,8 +61,8 @@ pub struct Diary {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),

sport-log-types/src/types/metcon.rs
@@ -6,6 +6,7 @@ use diesel::{sql_types::BigInt, PgConnection};
 #[cfg(feature = "server")]
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql,
@@ -21,9 +22,9 @@ use crate::{
     VerifyMultipleForUserOrAPCreate, VerifyMultipleForUserOrAPWithDb,
     VerifyMultipleForUserOrAPWithoutDb,
 };
-use crate::{Movement, MovementId, TrainingPlanId, UserId};
+use crate::{types::IdString, Movement, MovementId, TrainingPlanId, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "server", derive(DbEnum))]
 pub enum MetconType {
     Amrap,
@@ -31,7 +32,7 @@ pub enum MetconType {
     ForTime,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "server", derive(DbEnum))]
 pub enum DistanceUnit {
     Meter,
@@ -41,8 +42,8 @@ pub enum DistanceUnit {
     Mile,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -201,8 +202,8 @@ impl VerifyMultipleForUserOrAPWithoutDb for Unverified<Vec<Metcon>> {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -370,8 +371,8 @@ impl VerifyMultipleForUserOrAPCreate for Unverified<Vec<MetconMovement>> {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -419,8 +420,8 @@ pub struct MetconSession {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),

sport-log-types/src/types/mod.rs
@@ -4,8 +4,7 @@ use axum::http::StatusCode;
 use chrono::{DateTime, Utc};
 #[cfg(feature = "server")]
 use diesel::{PgConnection, QueryResult};
-#[cfg(feature = "server")]
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 mod account;
 mod action;
@@ -51,6 +50,10 @@ pub use strength::*;
 pub use training_plan::*;
 pub use user::*;
 pub use version::*;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(transparent)]
+struct IdString(String);
 
 /// Wrapper around incoming json data for which the access permissions for the [AuthUserOrAP], [AuthAP] or [AuthAdmin] have not been checked.
 ///

sport-log-types/src/types/movement.rs
@@ -5,13 +5,13 @@ use diesel::sql_types::BigInt;
 #[cfg(feature = "server")]
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetAll, GetById, GetByIds, HardDelete, IdFromSql, IdToSql, Update,
     VerifyForAdminWithoutDb, VerifyIdForAdmin, VerifyIdUnchecked,
 };
 
-use crate::UserId;
 #[cfg(feature = "server")]
 use crate::{
     schema::{eorm, movement, movement_muscle, muscle_group},
@@ -21,8 +21,9 @@ use crate::{
     VerifyMultipleForUserOrAPCreate, VerifyMultipleForUserOrAPWithDb,
     VerifyMultipleForUserOrAPWithoutDb,
 };
+use crate::{types::IdString, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "server", derive(DbEnum))]
 pub enum MovementDimension {
     Reps,
@@ -31,8 +32,8 @@ pub enum MovementDimension {
     Distance,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(
@@ -202,8 +203,8 @@ impl VerifyMultipleForUserOrAPWithoutDb for Unverified<Vec<Movement>> {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForAdmin),
@@ -223,8 +224,8 @@ pub struct MuscleGroup {
     pub description: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -385,8 +386,8 @@ impl VerifyMultipleForUserOrAPCreate for Unverified<Vec<MovementMuscle>> {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForAdmin),

sport-log-types/src/types/platform.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetAll, GetById, GetByIds, GetBySync, GetByUser, GetByUserSync,
@@ -8,15 +9,15 @@ use sport_log_types_derive::{
     VerifyForUserWithoutDb, VerifyIdForAdmin, VerifyIdForUser, VerifyIdUnchecked, VerifyUnchecked,
 };
 
-use crate::UserId;
 #[cfg(feature = "server")]
 use crate::{
     schema::{platform, platform_credential},
     User,
 };
+use crate::{types::IdString, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(
@@ -64,8 +65,8 @@ pub struct Platform {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUser),

sport-log-types/src/types/sharing.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql, IdToSql, Update,
@@ -14,10 +15,12 @@ use crate::{
     },
     CardioSession, Diary, MetconSession, StrengthSession, User,
 };
-use crate::{CardioSessionId, DiaryId, MetconSessionId, StrengthSessionId, UserId};
+use crate::{
+    types::IdString, CardioSessionId, DiaryId, MetconSessionId, StrengthSessionId, UserId,
+};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -48,8 +51,8 @@ pub struct Group {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -84,8 +87,8 @@ pub struct GroupUser {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -118,8 +121,8 @@ pub struct SharedMetconSession {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -152,8 +155,8 @@ pub struct SharedStrengthSession {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),
@@ -186,8 +189,8 @@ pub struct SharedCardioSession {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),

sport-log-types/src/types/strength.rs
@@ -4,6 +4,7 @@ use chrono::{DateTime, Utc};
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql, IdToSql, Update,
@@ -16,10 +17,10 @@ use crate::{
     AuthUserOrAP, CheckUserId, TrainingPlan, Unverified, User, VerifyForUserOrAPCreate,
     VerifyForUserOrAPWithDb, VerifyMultipleForUserOrAPCreate, VerifyMultipleForUserOrAPWithDb,
 };
-use crate::{Movement, MovementId, TrainingPlanId, UserId};
+use crate::{types::IdString, Movement, MovementId, TrainingPlanId, UserId};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -62,8 +63,8 @@ pub struct StrengthBlueprint {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -189,8 +190,8 @@ impl VerifyMultipleForUserOrAPCreate for Unverified<Vec<StrengthBlueprintSet>> {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),
@@ -234,8 +235,8 @@ pub struct StrengthSession {
     pub deleted: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),

sport-log-types/src/types/training_plan.rs
@@ -2,6 +2,7 @@ use chrono::NaiveDate;
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     CheckUserId, Create, GetById, GetByIds, GetByUser, GetByUserSync, HardDelete, IdFromSql,
@@ -10,10 +11,10 @@ use sport_log_types_derive::{
 
 #[cfg(feature = "server")]
 use crate::{schema::training_plan, User};
-use crate::{UserId, Weekday};
+use crate::{types::IdString, UserId, Weekday};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql, VerifyIdForUserOrAP),

sport-log-types/src/types/user.rs
@@ -3,16 +3,18 @@ use axum::http::StatusCode;
 #[cfg(feature = "server")]
 use diesel::sql_types::BigInt;
 use serde::{Deserialize, Serialize};
+use sport_log_types_derive::IdString;
 #[cfg(feature = "server")]
 use sport_log_types_derive::{
     GetById, GetByIds, IdFromSql, IdToSql, VerifyForAdminWithoutDb, VerifyUnchecked,
 };
 
+use crate::types::IdString;
 #[cfg(feature = "server")]
 use crate::{schema::user, AuthUser, CheckUserId, Unverified, VerifyForUserWithDb};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-#[serde(transparent)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, IdString)]
+#[serde(try_from = "IdString", into = "IdString")]
 #[cfg_attr(
     feature = "server",
     derive(Hash, FromSqlRow, AsExpression, IdToSql, IdFromSql),