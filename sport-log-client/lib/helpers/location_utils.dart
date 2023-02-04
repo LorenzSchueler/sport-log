8fabf14bb076a268cd0933f6ec9b843583cc30c9
Lorenz Schüler <lorenz@saarow.de>
2023-02-04 18:20:59 +0100

    fix: show message before opening location settings

--- sport-log-client/lib/helpers/location_utils.dart
+++ sport-log-client/lib/helpers/location_utils.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:location/location.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:sport_log/app.dart';
 import 'package:sport_log/helpers/extensions/location_data_extension.dart';
+import 'package:sport_log/widgets/dialogs/message_dialog.dart';
 import 'package:sport_log/widgets/dialogs/system_settings_dialog.dart';
 
 class LocationUtils {
@@ -36,16 +38,27 @@ class LocationUtils {
       while (!await Permission.locationWhenInUse.request().isGranted) {
         final ignore = await showSystemSettingsDialog(
           text:
-              "In order to track your location the permission needs to be 'allowed'",
+              "In order to track your location the permission needs to be 'allowed'.",
         );
         if (ignore) {
           return false;
         }
       }
+
+      if (!await Permission.locationAlways.isGranted) {
+        final context = App.globalContext;
+        if (context.mounted) {
+          await showMessageDialog(
+            context: context,
+            text:
+                "In order to track your location while the screen is off the permission needs to be set to 'always allow'.",
+          );
+        }
+      }
       while (!await Permission.locationAlways.request().isGranted) {
         final ignore = await showSystemSettingsDialog(
           text:
-              "In order to track your location while the screen is off the permission needs to be set to 'always allow'",
+              "In order to track your location while the screen is off the permission needs to be set to 'always allow'.",
         );
         if (ignore) {
           return false;
