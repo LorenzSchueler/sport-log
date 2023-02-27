3aa14113fae9c6fc049f1b37c82ee8dcf937110c
Lorenz Schüler <lorenz@saarow.de>
2023-02-27 15:11:05 +0100

    fix: disable elevation fetch from open elevation api

--- sport-log-client/lib/pages/workout/cardio/route_edit_page.dart
+++ sport-log-client/lib/pages/workout/cardio/route_edit_page.dart
@@ -134,9 +134,11 @@ class _RouteEditPageState extends State<RouteEditPage> {
       );
       return;
     }
-    final elevationResult = await RoutePlanningUtils.getElevations([location]);
-    final elevation =
-        elevationResult.isSuccess ? elevationResult.success[0] : 0;
+    // TODO currently the public API is overloaded and often returns with timeout
+    //final elevationResult = await RoutePlanningUtils.getElevations([location]);
+    //final elevation =
+    //elevationResult.isSuccess ? elevationResult.success[0] : 0;
+    const elevation = 0;
     if (mounted) {
       setState(() {
         _route.markedPositions!.add(
