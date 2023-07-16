b001c91b3db3abbfa3fbe5d2ae5be7331aa9b1e8
Lorenz Schüler <lorenz@saarow.de>
2023-07-16 14:24:24 +0200

    feat: use own polyline decode function

--- sport-log-client/lib/helpers/route_planning_utils.dart
+++ sport-log-client/lib/helpers/route_planning_utils.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:collection/collection.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:mapbox_api/mapbox_api.dart';
 import 'package:result_type/result_type.dart';
 import 'package:sport_log/defaults.dart';
@@ -90,10 +90,7 @@ class RoutePlanningUtils {
   ) async {
     final navRoute = response.routes![0];
     final track = <Position>[];
-    final latLngs = PolylinePoints()
-        .decodePolyline(navRoute.geometry as String)
-        .map((p) => LatLng(lat: p.latitude, lng: p.longitude))
-        .toList();
+    final latLngs = _decodePolyline(navRoute.geometry as String);
     for (final latLng in latLngs) {
       final elevation = await getElevation?.call(latLng);
       track.add(
@@ -109,6 +106,57 @@ class RoutePlanningUtils {
       );
     }
     return track;
+  }
+
+  // https://developers.google.com/maps/documentation/utilities/polylinealgorithm
+  static List<LatLng> _decodePolyline(
+    String polyline, {
+    int accuracyExponent = 5,
+  }) {
+    final accuracyMultiplier = pow(10, accuracyExponent);
+    final latLngs = <LatLng>[];
+
+    var index = 0;
+    var lat = 0;
+    var lng = 0;
+
+    while (index < polyline.length) {
+      // Return difference to next value (latitude or longitude)
+      int getNextDiff() {
+        var shift = 0;
+        var value = 0;
+        int byte, chunk;
+
+        // Iterating while value is grater or equal of `32-bits` size
+        do {
+          // convert char to int
+          byte = polyline.codeUnitAt(index++);
+          // subtract 63
+          byte -= 63;
+          // AND byte with 0x1f to undo OR with 0x20
+          chunk = byte & 0x1f;
+          // add next 5 bits chunks to value
+          value |= chunk << shift;
+          shift += 5;
+          // check if another byte follows (byte & 0x20 > 0)
+        } while (byte >= 0x20);
+
+        // right-shift value by one bit
+        // invert value if negative
+        return (value & 1) != 0
+            ? (~BigInt.from(value >> 1)).toInt()
+            : value >> 1;
+      }
+
+      lat += getNextDiff();
+      lng += getNextDiff();
+
+      latLngs.add(
+        LatLng(lat: lat / accuracyMultiplier, lng: lng / accuracyMultiplier),
+      );
+    }
+
+    return latLngs;
   }
 
   static Future<Result<List<Position>, RoutePlanningError>> matchLocations(
