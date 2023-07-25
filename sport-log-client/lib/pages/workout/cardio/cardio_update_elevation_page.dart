b7b8bbd0c10e4afd61b0a31250e23d4acdec3bb3
Lorenz Schüler <lorenz@saarow.de>
2023-07-25 11:43:54 +0200

    feat: show elevation chart on CardioUpdateElevationPage and provide user with option to cancel after change

--- sport-log-client/lib/pages/workout/cardio/cardio_update_elevation_page.dart
+++ sport-log-client/lib/pages/workout/cardio/cardio_update_elevation_page.dart
@@ -1,8 +1,11 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart' hide Route;
 import 'package:sport_log/defaults.dart';
 import 'package:sport_log/helpers/map_controller.dart';
 import 'package:sport_log/helpers/page_return.dart';
 import 'package:sport_log/models/cardio/cardio_session_description.dart';
+import 'package:sport_log/models/cardio/position.dart';
+import 'package:sport_log/pages/workout/charts/duration_chart.dart';
 import 'package:sport_log/widgets/app_icons.dart';
 import 'package:sport_log/widgets/dialogs/dialogs.dart';
 import 'package:sport_log/widgets/map_widgets/static_mapbox_map.dart';
@@ -29,17 +32,33 @@ class _CardioUpdateElevationPageState extends State<CardioUpdateElevationPage> {
 
   ElevationMapController? _elevationMapController;
   double? _progress;
+  bool _changed = false;
+
+  static const _elevationColor = Color.fromARGB(255, 170, 130, 100);
+  late final DurationChartLine _elevationLine = _getElevationLine();
+  DurationChartLine _getElevationLine() =>
+      DurationChartLine.fromValues<Position>(
+        values: _cardioSessionDescription.cardioSession.track,
+        getDuration: (position) => position.time,
+        getGroupValue: (positions, _) =>
+            positions.map((p) => p.elevation).average,
+        lineColor: _elevationColor,
+        absolute: false,
+      );
+
+  static const _updatedElevationColor = Colors.red;
+  late DurationChartLine _updatedElevationLine = _getUpdatedElevationLine();
+  DurationChartLine _getUpdatedElevationLine() =>
+      DurationChartLine.fromValues<Position>(
+        values: _cardioSessionDescription.cardioSession.track,
+        getDuration: (position) => position.time,
+        getGroupValue: (positions, _) =>
+            positions.map((p) => p.elevation).average,
+        lineColor: _updatedElevationColor,
+        absolute: false,
+      );
 
   Future<void> _updateElevation() async {
-    final approved = await showApproveDialog(
-      context: context,
-      title: "Update Elevation",
-      text:
-          "The old elevation data will be permanently lost. The new elevations is taken from the map. This requires internet or offline maps.",
-    );
-    if (!approved) {
-      return;
-    }
     final track = _cardioSessionDescription.cardioSession.track;
     if (track != null) {
       for (var i = 0; i < track.length; i++) {
@@ -64,13 +83,21 @@ class _CardioUpdateElevationPageState extends State<CardioUpdateElevationPage> {
     }
     setState(() {
       _cardioSessionDescription.cardioSession.setAscentDescent();
+      _updatedElevationLine = _getUpdatedElevationLine();
+      _progress = null;
+      _changed = true;
     });
-    if (mounted) {
+  }
+
+  void _return({required bool apply}) {
+    if (apply) {
       Navigator.pop(
         context,
         // needed for cardio edit page
         ReturnObject.updated(_cardioSessionDescription),
       );
+    } else {
+      Navigator.pop(context);
     }
   }
 
@@ -83,30 +110,57 @@ class _CardioUpdateElevationPageState extends State<CardioUpdateElevationPage> {
           padding: Defaults.edgeInsets.normal,
           child: Column(
             children: [
+              Expanded(
+                child: DurationChart(
+                  chartLines: [
+                    _elevationLine,
+                    if (_changed) _updatedElevationLine,
+                  ],
+                ),
+              ),
               ElevationMap(
                 onMapCreated: (x) => _elevationMapController = x,
               ),
               if (_progress != null) LinearProgressIndicator(value: _progress),
               Defaults.sizedBox.vertical.normal,
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton.icon(
-                  icon: const Icon(AppIcons.trendingUp),
-                  label: const Text("Update Elevation"),
-                  onPressed: _progress == null ? _updateElevation : null,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.error,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton.icon(
-                  icon: const Icon(AppIcons.close),
-                  label: const Text("Cancel"),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              )
+              _changed
+                  ? Row(
+                      children: [
+                        Expanded(
+                          child: FilledButton.icon(
+                            icon: const Icon(AppIcons.close),
+                            label: const Text("Cancel"),
+                            onPressed: () => _return(apply: false),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                Theme.of(context).colorScheme.errorContainer,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Defaults.sizedBox.horizontal.normal,
+                        Expanded(
+                          child: FilledButton.icon(
+                            icon: const Icon(AppIcons.check),
+                            label: const Text("Apply"),
+                            onPressed: () => _return(apply: true),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                Theme.of(context).colorScheme.error,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  : SizedBox(
+                      width: double.infinity,
+                      child: FilledButton.icon(
+                        icon: const Icon(AppIcons.trendingUp),
+                        label: const Text("Update Elevation"),
+                        onPressed: _progress == null ? _updateElevation : null,
+                      ),
+                    )
             ],
           ),
         ),
