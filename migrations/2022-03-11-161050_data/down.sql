ffeca7ba1f90e78f532b25ab545345c3de3df1bd
Lorenz Schüler <lorenz@saarow.de>
2023-10-07 14:40:42 +0200

    fix: migrations

--- migrations/2022-03-11-161050_data/down.sql
+++ migrations/2022-03-11-161050_data/down.sql
@@ -1,15 +1,10 @@
 delete from cardio_session;
-delete from cardio_blueprint;
 delete from route;
-delete from metcon_item;
 delete from metcon_session;
 delete from metcon_movement;
 delete from metcon;
 delete from strength_set;
 delete from strength_session;
-delete from strength_blueprint_set;
-delete from strength_blueprint;
-delete from training_plan;
 delete from eorm;
 delete from movement_muscle;
 delete from muscle_group;
