bbba6f10957b97af928b3527268a41413f807259
Lorenz Schüler <lorenz@saarow.de>
2024-02-14 09:14:36 +0100

    fix: get elevation by removing Offstage wrapper

--- sport-log-client/lib/widgets/map_widgets/static_mapbox_map.dart
+++ sport-log-client/lib/widgets/map_widgets/static_mapbox_map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' hide Settings;
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart'
+    hide Settings, Visibility;
 import 'package:provider/provider.dart';
 import 'package:sport_log/helpers/lat_lng.dart';
 import 'package:sport_log/helpers/map_controller.dart';
@@ -48,17 +49,19 @@ class ElevationMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Offstage(
-      child: SizedBox(
-        height: 1,
-        width: 1,
-        child: StaticMapboxMap(
-          onMapCreated: (mapController) async {
-            await mapController.setZoom(15);
-            await mapController.enableTerrain("elevation-terrain-source", 0);
-            onMapCreated(ElevationMapController(mapController));
-          },
-        ),
+    // TODO avoid rendering
+    // currently only works when rendered
+    // it worked for some time when wrapped in Offstage but then stopped reporting elevation
+    // also does not work when wrapped in Visibility(visible: false) or Opacity(opacity: 0)
+    return SizedBox(
+      height: 1,
+      width: 1,
+      child: StaticMapboxMap(
+        onMapCreated: (mapController) async {
+          await mapController.setZoom(15);
+          await mapController.enableTerrain("elevation-terrain-source", 0);
+          onMapCreated(ElevationMapController(mapController));
+        },
       ),
     );
   }
