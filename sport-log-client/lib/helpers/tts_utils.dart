808288f46d169fafab537cfad6a9789dffcea5a3
Lorenz Schüler <lorenz@saarow.de>
2024-06-17 16:28:40 +0200

    fix: comment out tts code, should be fixed at a later time

--- sport-log-client/lib/helpers/tts_utils.dart
+++ sport-log-client/lib/helpers/tts_utils.dart
@@ -8,55 +8,57 @@ class TtsUtils {
   factory TtsUtils() {
     final instance = TtsUtils._();
 
-    Future(() async {
-      if (await instance._tts.getDefaultEngine == null) {
-        final engines = (await instance._tts.getEngines) as List;
-        if (engines.isNotEmpty) {
-          await instance._tts.setEngine(engines[0] as String);
-        }
-      }
+    //Future(() async {
+    //if (await instance._tts.getDefaultEngine == null) {
+    //final engines = (await instance._tts.getEngines) as List;
+    //if (engines.isNotEmpty) {
+    //await instance._tts.setEngine(engines[0] as String);
+    //}
+    //}
 
-      await instance._tts.setVoice({
-        "name": "en-US-language",
-        "locale": "en-US",
-      }); // ignored if not available
-      await instance._tts.awaitSpeakCompletion(true);
-    });
+    //await instance._tts.setVoice({
+    //"name": "en-US-language",
+    //"locale": "en-US",
+    //}); // ignored if not available
+    //await instance._tts.awaitSpeakCompletion(true);
+    //});
 
-    // ignore: prefer-async-await
-    AudioSession.instance.then((session) {
-      instance._session = session;
+    //// ignore: prefer-async-await
+    //AudioSession.instance.then((session) {
+    //instance._session = session;
 
-      session.configure(
-        const AudioSessionConfiguration(
-          avAudioSessionCategory: AVAudioSessionCategory.playback,
-          avAudioSessionCategoryOptions:
-              AVAudioSessionCategoryOptions.duckOthers,
-          avAudioSessionMode: AVAudioSessionMode.spokenAudio,
-          avAudioSessionSetActiveOptions:
-              AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation,
-          androidAudioAttributes: AndroidAudioAttributes(
-            contentType: AndroidAudioContentType.speech,
-            usage: AndroidAudioUsage.assistanceNavigationGuidance,
-          ),
-          androidAudioFocusGainType:
-              AndroidAudioFocusGainType.gainTransientMayDuck,
-          androidWillPauseWhenDucked: true,
-        ),
-      );
-    });
+    //session.configure(
+    //const AudioSessionConfiguration(
+    //avAudioSessionCategory: AVAudioSessionCategory.playback,
+    //avAudioSessionCategoryOptions:
+    //AVAudioSessionCategoryOptions.duckOthers,
+    //avAudioSessionMode: AVAudioSessionMode.spokenAudio,
+    //avAudioSessionSetActiveOptions:
+    //AVAudioSessionSetActiveOptions.notifyOthersOnDeactivation,
+    //androidAudioAttributes: AndroidAudioAttributes(
+    //contentType: AndroidAudioContentType.speech,
+    //usage: AndroidAudioUsage.assistanceNavigationGuidance,
+    //),
+    //androidAudioFocusGainType:
+    //AndroidAudioFocusGainType.gainTransientMayDuck,
+    //androidWillPauseWhenDucked: true,
+    //),
+    //);
+    //});
 
     return instance;
   }
 
   TtsUtils._();
 
+  // ignore: unused_field
   final FlutterTts _tts = FlutterTts();
+  // ignore: unused_field, use_late_for_private_fields_and_variables
   AudioSession? _session;
 
   Future<void> speak(String text) async {
-    await _session?.setActive(true);
-    await _tts.speak(text);
-    await _session?.setActive(false);
+    //await _session?.setActive(true);
+    //await _tts.speak(text);
+    //await _session?.setActive(false);
   }
 }
