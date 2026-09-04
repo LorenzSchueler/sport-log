7d9159e4b426baf830a8db52ee7dea23916a774d
Lorenz Schüler <lorenz@saarow.de>
2026-09-04 22:11:50 +0200

    build: update platform files by regenerating them

--- sport-log-client/android/app/build.gradle.kts
+++ sport-log-client/android/app/build.gradle.kts
@@ -14,7 +14,8 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    compileSdk = 36 // flutter.compileSdkVersion
+    namespace = "org.sport_log.sport_log_client"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -23,15 +24,13 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    namespace = "org.sport_log.sport_log_client"
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "org.sport_log.sport_log_client"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 29 // flutter.minSdkVersion
-        targetSdk = 36 // flutter.targetSdkVersion
+        minSdk = 29
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -72,14 +71,14 @@ android {
     }
 }
 
-flutter {
-    source = "../.."
-}
-
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+flutter {
+    source = "../.."
 }
 
 dependencies {
--- sport-log-client/android/app/src/main/kotlin/org/sport_log/sport_log_client/MainActivity.kt
+++ sport-log-client/android/app/src/main/kotlin/org/sport_log/sport_log_client/MainActivity.kt
@@ -2,5 +2,4 @@ package org.sport_log.sport_log_client
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
